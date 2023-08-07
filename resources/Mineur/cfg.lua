67a50af7baf3f3953410826e34a746d47ff66dfe
totald5 <pellouinalexandre@gmail.com>
2023-08-07 13:20:13 +0200

    update cg.lua

--- resources/Mineur/cfg.lua
+++ resources/Mineur/cfg.lua
@@ -1,10 +1,9 @@
 Config = {}
 
-Config.MinerJobCoalPos = { vector3(2797.434, 1395.36, 70.32012)} -- Pos du charbon
-Config.MinerJobRubblePos = {x = 2791.935, y = 1337.352, z = 71.329} -- Pos des gravats
-Config.MinerJobDepositPos = { vector3(2880.175, 1401.344, 68.70103)} -- 
-Config.MinerJobWithdrawalPos = { vector3(2954.671, 1374.939, 51.32298)}
-Config.DistanceToInteract = 2.2
+Config.MinerJobCoalPos = vector3(2797.434, 1395.36, 70.32012) -- Pos du charbon
+Config.MinerJobDepositPos = vector3(2880.175, 1401.344, 68.70103)
+Config.MinerJobWithdrawPos = vector3(2954.671, 1374.939, 51.32298)
+Config.DistanceToInteract = 1.5
 
 Config.PointSprite = 2033377404
 
@@ -28,14 +27,6 @@ Config.WorkingTimeCoal = 5000
 Config.TimerMsg = " Currently working for :"
 
 Config.ShowBlips = true
-
---[[
-Config.p1 = vector3(2754.291, 1358.377, 68.368)
-Config.p2 = vector3(2753.534, 1367.671, 67.804)
-Config.p3 = vector3(2758.344, 1371.043, 68.042)
-Config.p4 = vector3(2767.227, 1371.093, 67.987)
-Config.p5 = vector3(2767.958, 1382.240, 67.929)
---]]
 
 Config.RessourcesPoints = 
 {
