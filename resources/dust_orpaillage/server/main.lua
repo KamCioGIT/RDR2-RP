af251b127db3fe1138c1c5bf726e7c941e1c06da
totald5 <pellouinalexandre@gmail.com>
2023-08-11 21:15:02 +0200

    Ajout Orpaillage

---
+++ resources/dust_orpaillage/server/main.lua
@@ -0,0 +1,23 @@
+RedEM = exports["redem_roleplay"]:RedEM()
+
+data = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    data = call
+end)
+
+RegisterServerEvent("dust-or:server:récolte", function(riverdrop, riverdrop2)
+    local Player = RedEM.GetPlayer(source)
+    if Player then
+        local roll = math.random(1, 100)
+        if roll <= riverdrop then
+            local roll2 = math.random(1,100)
+            local amount = 1
+            if roll2 <= riverdrop2 then                
+                amount = 2
+            end
+
+            local ItemData = data.getItem(source, 'pepiteor')
+	        ItemData.AddItem(amount)
+        end
+    end
+end)
