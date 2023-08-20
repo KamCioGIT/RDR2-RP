9f4588632ea207b63eafa4a04645ba63c4b74e46
Aurélien <cordaurelien@gmail.com>
2023-08-20 19:39:26 +0200

    Update server.lua

--- resources/[MyJobs]/dust_usine/server.lua
+++ resources/[MyJobs]/dust_usine/server.lua
@@ -44,8 +44,8 @@ RegisterServerEvent("usine:MaxRessourcesAmount", function(dataType)
     local ItemData = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe1Name)
     local ItemData2 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe2Name)
     
-    local rItem1Amount = ItemData.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe1Amount
-    local rItem2Amount = ItemData2.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe2Amount
+    local rItem1Amount = tonumber(ItemData.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe1Amount)
+    local rItem2Amount = tonumber(ItemData2.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe2Amount)
     
     print(rItem1Amount)
     print(rItem2Amount)
@@ -57,6 +57,7 @@ RegisterServerEvent("usine:MaxRessourcesAmount", function(dataType)
         end
     end
     
+	print(maxCraftingItemNbr)
     return maxCraftingItemNbr
 end)
 
