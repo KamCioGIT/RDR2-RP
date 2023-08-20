456f4171af8ea4097e48d904b38d79c2c65ec570
levraimurphy <pellouinalexandre@gmail.com>
2023-08-20 02:39:55 +0200

    Update main.lua

--- resources/[MyJobs]/dust_orpaillage/server/main.lua
+++ resources/[MyJobs]/dust_orpaillage/server/main.lua
@@ -27,17 +27,13 @@ AddEventHandler('dust-or:server:ramp', function()
 	local _source = source
 	-- TriggerEvent("redemrp_inventory:getData", function(Inventory)
 	local ItemData = data.getItem(_source, "cailloux")
-	Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(Config.WorkingTime)
-            if ItemData.RemoveItem(2) then
-                TriggerClientEvent("dust-or:server:rampanim", _source)
-                local ItemDatagive = data.getItem(_source, "pepiteor")
-                local roll = math.random(0, 100)
-                ItemDatagive.AddItem(1)
-            end
-        end
-    end)
+	while ItemData.RemoveItem(2) do
+        TriggerNetEvent("dust-or:server:rampanim", _source)
+        Citizen.Wait(Config.WorkingTime)
+        local ItemDatagive = data.getItem(_source, "pepiteor")
+        local roll = math.random(0, 100)
+        ItemDatagive.AddItem(1)
+	end
 end)
 
 RegisterServerEvent("RegisterUsableItem:goldramp")
