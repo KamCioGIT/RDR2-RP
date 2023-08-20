94fe13b4c4e24f55094fdd249b89392b177638fe
levraimurphy <pellouinalexandre@gmail.com>
2023-08-21 00:36:27 +0200

    Update server.lua

--- resources/dust_vault/server.lua
+++ resources/dust_vault/server.lua
@@ -63,7 +63,7 @@ AddEventHandler("dust_vault:server:vaultDB", function(vault, x, y, z)
 		charid = charid,
 		stashid = generetedUid,
 		model = vault
-		coords = json.encode(vaultcoords)
+		coords = json.encode(vaultcoords) 
 	}, function(rowsChanged)
 	end)
 end)
