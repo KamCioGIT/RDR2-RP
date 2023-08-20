d384c7786553f0020fde93a3f2c10c221403f0b2
levraimurphy <pellouinalexandre@gmail.com>
2023-08-20 20:08:39 +0200

    Update main.lua

--- resources/[MyJobs]/dust_orpaillage/client/main.lua
+++ resources/[MyJobs]/dust_orpaillage/client/main.lua
@@ -67,10 +67,10 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
         local playerPed = PlayerPedId()
         local pos = GetEntityCoords(playerPed), true
-        local goldramp = GetClosestObjectOfType(pos, 2.0, Config.GoldRamp, false, false, false)
+        local goldramp = GetClosestObjectOfType(pos, 1.5, Config.GoldRamp, false, false, false)
         if goldramp ~= 0 then
             local objectPos = GetEntityCoords(goldramp)
-            if #(pos - objectPos) < 1.0 and not isInteracting then
+            if #(pos - objectPos) < 1.5 and not isInteracting then
                 PromptSetActiveGroupThisFrame(RampPromptGroup, RampPromptName)
             end
             if IsControlJustReleased(0, 0x5181713D) and not isInteracting then
