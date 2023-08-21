8a49417aaeff3ad8ea7185b7194f78806c4a0f16
levraimurphy <pellouinalexandre@gmail.com>
2023-08-21 19:37:12 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -33,9 +33,9 @@ AddEventHandler("dust_vault:server:getcoords", function (coords)
     local playerpos = GetEntityCoords(playerPed)
     print (coords)
     print ("getcoords")
-    for k,v in ipairs(coords) do
+    for k,v in pairs(coords) do
         if #(playerpos - v) < 200 then
-            print (k, v)
+            print ("ninho")
             TriggerServerEvent("dust_vault:server:AskModel", v)
         end
     end
