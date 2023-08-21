7cdcb1b5d0d2a3909c2fe6744f712d6d30169e88
levraimurphy <pellouinalexandre@gmail.com>
2023-08-22 01:50:58 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -42,7 +42,7 @@ AddEventHandler("dust_vault:server:getmodel", function (model, heading, coords, 
     local vaultpos = vector3(coords.x, coords.y, coords.z)
     local limit = 0
     print 'spawn'
-    print ((model, heading, coords, id))
+    print (model, heading, coords, id)
     local prop = CreateObject(tostring(model), coords.x, coords.y, coords.z, true, false, true)
     SetEntityHeading(prop, heading)
     PlaceObjectOnGroundProperly(prop)
