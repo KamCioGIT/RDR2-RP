a65f9f78b7c7d9cc2c15bae0eae9e87a50e6e499
levraimurphy <pellouinalexandre@gmail.com>
2023-08-21 17:10:25 +0200

    Update server.lua

--- resources/dust_vault/server.lua
+++ resources/dust_vault/server.lua
@@ -32,8 +32,7 @@ Citizen.CreateThread(function()
 					local coords = json.decode(result[i].coords)
 					local model = result[i].model
 					local prop = CreateObject(Config.SmallVault, coords.x, coords.y, coords.z, true, true, true)
-					-- SetEntityRotation(prop, 0.0, 0.0, -90.0, 0, true)
-					-- GetEntityRotation(prop)
+					SetEntityHeading(prop, -90.0)
 				end                    
 			end
 		end)
