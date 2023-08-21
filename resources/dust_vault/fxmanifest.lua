a3aa2340883a60fa9724a86e47bc8f835b371e65
levraimurphy <pellouinalexandre@gmail.com>
2023-08-21 12:24:48 +0200

    Update fxmanifest.lua

--- resources/dust_vault/fxmanifest.lua
+++ resources/dust_vault/fxmanifest.lua
@@ -3,6 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 server_script {
+	'@oxmysql/lib/MySQL.lua',
 	'server.lua',
 	'config.lua'
 }
