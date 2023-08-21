eafc60eca8410ad09cc5e71cd5165a9e1f2f9513
levraimurphy <pellouinalexandre@gmail.com>
2023-08-21 22:15:56 +0200

    fix

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -36,20 +36,26 @@ AddEventHandler("dust_vault:server:getcoords", function (coords)
     end
 end)
 
+local spawnedvault = {}
 RegisterNetEvent("dust_vault:server:getmodel")
-AddEventHandler("dust_vault:server:getmodel", function (model, heading, coords)
+AddEventHandler("dust_vault:server:getmodel", function (model, heading, coords, id)
     local playerPos = GetEntityCoords(PlayerPedId())
     local vaultpos = vector3(coords.x, coords.y, coords.z)
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1000)
-            if #(playerPos - vaultpos) < 10.0 then
-                if not prop then
-                    print 'spawn'
-                    local prop = CreateObject(Config.SmallVault, coords.x, coords.y, coords.z, true, false, true)
-                    SetEntityHeading(prop, heading)
-                    PlaceObjectOnGroundProperly(prop)
-                end
+            for k, v in ipairs(spawnedvault) do
+                if id ~= v then
+                    if #(playerPos - vaultpos) < 10.0 then
+                        if not prop then
+                            print 'spawn'
+                            local prop = CreateObject(Config.SmallVault, coords.x, coords.y, coords.z, true, false, true)
+                            SetEntityHeading(prop, heading)
+                            PlaceObjectOnGroundProperly(prop)
+                            table.insert(spawnedvault, id)
+                        end
+                    end
+                else return end
             end
         end
     end)
--- resources/dust_vault/server.lua
+++ resources/dust_vault/server.lua
@@ -41,14 +41,15 @@ AddEventHandler("dust_vault:server:AskModel", function (vaultcoords)
 	local _source = source
 	local coords = json.encode(vaultcoords)
 	print (coords)
-	MySQL.query('SELECT `model`,`heading` FROM `vault` WHERE `coords`=@coords ;',{coords = coords}, function(result)
+	MySQL.query('SELECT `id`,`model`,`heading` FROM `vault` WHERE `coords`=@coords ;',{coords = coords}, function(result)
 		if #result ~= 0 then
 			print ("result")
 			for i = 1, #result do
+				local id = id
 				local model = result[i].model
 				local heading = result[i].heading
 				local coords = json.decode(coords)
-				TriggerClientEvent("dust_vault:server:getmodel", _source, model, heading, coords)
+				TriggerClientEvent("dust_vault:server:getmodel", _source, model, heading, coords, id)
 			end                    
 		end
 	end)
