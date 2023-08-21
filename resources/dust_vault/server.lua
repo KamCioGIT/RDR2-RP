7404ed90bcfcb62f9a6764d171464c38e6b225ee
levraimurphy <pellouinalexandre@gmail.com>
2023-08-21 13:13:07 +0200

    Update server.lua

--- resources/dust_vault/server.lua
+++ resources/dust_vault/server.lua
@@ -25,7 +25,7 @@ end)
 ---- RECUP LES POS ET LE PROP---
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(0)
+		Citizen.Wait(50)
 		MySQL.query('SELECT `coords`, `model` FROM `vault`;',{}, function(result)
 			if #result ~= 0 then
 				for i = 1, #result do
