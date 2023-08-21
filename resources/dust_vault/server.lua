965c1b531f407278d087720505df80a36c8aebbd
levraimurphy <pellouinalexandre@gmail.com>
2023-08-22 01:20:45 +0200

    Update server.lua

--- resources/dust_vault/server.lua
+++ resources/dust_vault/server.lua
@@ -47,10 +47,6 @@ AddEventHandler("dust_vault:server:Askcoords", function()
 				local model = result[i].model
 				local prop = Citizen.InvokeNative(0x2F7AA05C, model, coords.x, coords.y, coords.z, true, true, true)
 				print (coords, heading, model)
-				if prop then
-					print "yes"
-					-- SetEntityHeading(prop, heading)
-				end
 			end                    
 		end
 	end)
