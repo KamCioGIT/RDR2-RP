14e4d6df673494a33fb5c7895097c0f1a95779f2
levraimurphy <pellouinalexandre@gmail.com>
2023-08-22 20:20:36 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -264,8 +264,8 @@ function posecoffre(model)
     local vault = model
     local playerPed = PlayerPedId()
     PoseCoffrePromptShown = false
-    -- Citizen.CreateThread(function()
-    --     while true do
+    Citizen.CreateThread(function()
+        while true do
             Citizen.Wait(0)
             if PoseCoffrePromptShown == false then
                 PromptSetEnabled(LeavePrompt, true)
@@ -276,6 +276,7 @@ function posecoffre(model)
             ---- Lancer anim porter une caisse
             local playerpos = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.5, 0)
             Citizen.InvokeNative(0x2A32FAA57B937173, -1795314153, playerpos.x, playerpos.y, playerpos.z - 1.0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0)
+        end
             if PromptHasHoldModeCompleted(LeavePrompt) then
                 ---- cancel anim
                 PoseCoffrePromptShown = false
@@ -337,7 +338,6 @@ function posecoffre(model)
                 ---- cancel anim
                 return
             end
-    --     end
-    -- end)
+    end)
 end
 
