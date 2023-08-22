60db8f3b0c32fe9900a9a1de5a961fde68a8f413
levraimurphy <pellouinalexandre@gmail.com>
2023-08-22 02:29:45 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -1,5 +1,25 @@
 RedEM = exports["redem_roleplay"]:RedEM()
 
+local CraftMenuPrompt = nil
+local CraftMenuPromptShown = false
+local promptGroup
+
+local varString = CreateVarString(10, "LITERAL_STRING", "Poser le coffre")
+
+Citizen.CreateThread(function()
+    Wait(10)
+    CraftMenuPrompt = PromptRegisterBegin()
+    PromptSetActiveGroupThisFrame(promptGroup, varString)
+    PromptSetControlAction(CraftMenuPrompt, 0xE8342FF2) -- LEFT ALT
+    PromptSetText(CraftMenuPrompt, CreateVarString(10, "LITERAL_STRING", "Poser le coffre"))
+    PromptSetStandardMode(CraftMenuPrompt, true)
+    PromptSetEnabled(CraftMenuPrompt, false)
+    PromptSetVisible(CraftMenuPrompt, false)
+    Citizen.InvokeNative(0x94073D5CA3F16B7B, CraftMenuPrompt, 1000)
+    N_0x0c718001b77ca468(CraftMenuPrompt, 2.0)
+    PromptSetGroup(CraftMenuPrompt, promptGroup)
+    PromptRegisterEnd(CraftMenuPrompt)
+end)
 
 ----- REQUEST LES MODEL ----
 
@@ -55,23 +75,32 @@ RegisterNetEvent('smallvault')
 AddEventHandler('smallvault', function() 
     local vault = Config.SmallVault
     local playerPed = PlayerPedId()
-    RequestAnimDict(Config.MenuDict)
-    while not HasAnimDictLoaded(Config.MenuDict) do
-        Citizen.Wait(50)
-    end
-    for k,v in pairs(Config.MenuAnim) do
-        TaskPlayAnim(playerPed, Config.MenuDict, v, 8.0, -8.0, -1, 2, 0, true)
-    end
-    Citizen.Wait(3000)
-    RequestAnimDict(Config.CloseMenuDict)
-    while not HasAnimDictLoaded(Config.CloseMenuDict) do
-        Citizen.Wait(50)
-    end
-    for k,v in pairs(Config.CloseMenuAnim) do
-        TaskPlayAnim(playerPed, Config.CloseMenuDict, v, 8.0, -8.0, -1, 0, 0, true)
-        Citizen.Wait(1000)
-    end
-    local playerpos = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.5, -1.55)
-    local heading = GetEntityHeading(PlayerPedId())
-    TriggerServerEvent("dust_vault:server:vaultDB", vault, playerpos, heading) -- Créer le vault dans la db
-end, false)
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(50)
+            local playerpos = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.5, -1.55)
+            Citizen.InvokeNative(0x2A32FAA57B937173, -1795314153, playerpos.x, playerpos.y, playerpos.z - 1.0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0)
+            if PromptHasHoldModeCompleted(CraftMenuPrompt) then
+                RequestAnimDict(Config.MenuDict)
+                while not HasAnimDictLoaded(Config.MenuDict) do
+                    Citizen.Wait(50)
+                end
+                for k,v in pairs(Config.MenuAnim) do
+                    TaskPlayAnim(playerPed, Config.MenuDict, v, 8.0, -8.0, -1, 2, 0, true)
+                end
+                Citizen.Wait(3000)
+                RequestAnimDict(Config.CloseMenuDict)
+                while not HasAnimDictLoaded(Config.CloseMenuDict) do
+                    Citizen.Wait(50)
+                end
+                for k,v in pairs(Config.CloseMenuAnim) do
+                    TaskPlayAnim(playerPed, Config.CloseMenuDict, v, 8.0, -8.0, -1, 0, 0, true)
+                    Citizen.Wait(1000)
+                end
+                local heading = GetEntityHeading(PlayerPedId())
+                TriggerServerEvent("dust_vault:server:vaultDB", vault, playerpos, heading) -- Créer le vault dans la db
+                return
+            end
+        end
+    end)
+end)
