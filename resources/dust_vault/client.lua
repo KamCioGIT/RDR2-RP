476a1f2859a8ad56bd4c322550fa3b023ef8879d
levraimurphy <pellouinalexandre@gmail.com>
2023-08-23 14:53:04 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -248,16 +248,14 @@ AddEventHandler("dust_vault:server:getmodel", function (model, heading, coords)
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1000)
-            if #(playerPos - vaultpos) < 10.0 then
-                for k, v in ipairs(coordscache) do
-                    print(#coordscache)
-                    if v == vaultpos then 
-                        local prop = CreateObject(model, coords.x, coords.y, coords.z, false, true, true)
-                        SetEntityHeading(prop, tonumber(heading))
-                        PlaceObjectOnGroundProperly(prop)
-                        table.remove(coordscache, k)
-                        print "spawn"
-                    end
+            for k, v in ipairs(coordscache) do
+                print(#coordscache)
+                if #(playerPos - v) < 10.0 then
+                    local prop = CreateObject(model, coords.x, coords.y, coords.z, false, true, true)
+                    SetEntityHeading(prop, tonumber(heading))
+                    PlaceObjectOnGroundProperly(prop)
+                    table.remove(coordscache, k)
+                    print "spawn"
                 end
             end
         end
