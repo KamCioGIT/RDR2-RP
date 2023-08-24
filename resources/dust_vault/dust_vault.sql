5e0ce27f1166c9e34f8dd4f0cba9ebd906d44620
totald5 <pellouinalexandre@gmail.com>
2023-08-24 19:51:20 +0200

    Update dust_vault.sql

--- resources/dust_vault/dust_vault.sql
+++ resources/dust_vault/dust_vault.sql
@@ -1,14 +1,36 @@
-CREATE TABLE `vault` (
-  `id` int(11) NOT NULL,
-  `identifier` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           10.4.28-MariaDB - mariadb.org binary distribution
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.5.0.6677
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Listage de la structure de la table redemrpframework_cd31cb. vault
+CREATE TABLE IF NOT EXISTS `vault` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(50) DEFAULT NULL,
   `charid` int(11) DEFAULT 0,
   `stashid` text NOT NULL,
-  `model` text NOT NULL,
-  `coords` varchar(200) COLLATE utf8mb4_bin DEFAULT '{}'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `model` int(11) NOT NULL DEFAULT 0,
+  `coords` varchar(200) DEFAULT '{}',
+  `heading` varchar(200) DEFAULT '{}',
+  `code` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-ALTER TABLE `vault`
-  ADD PRIMARY KEY (`id`);
+-- Les données exportées n'étaient pas sélectionnées.
 
-ALTER TABLE `vault`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
