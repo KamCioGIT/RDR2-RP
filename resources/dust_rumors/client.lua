80af7fcf3257b14b752c1f9cac924c0ae4fc8625
Aurélien <cordaurelien@gmail.com>
2023-08-25 16:26:26 +0200

    Update client.lua

--- resources/dust_rumors/client.lua
+++ resources/dust_rumors/client.lua
@@ -16,7 +16,7 @@ Citizen.CreateThread(function()
 
                 boneCoord = GetWorldPositionOfEntityBone(entity, 31086)
                 coords = entityPos + boneCoord
-                DrawText3D(coords.x, coords.y, coords.z + 0.5, "MONSTRE")
+                DrawText3D(coords.x, coords.y, coords.z + 1, "MONSTRE")
             end
         end
 
