65eac379b45be9258be59ed343d28d2bcd232a6a
Aurélien <cordaurelien@gmail.com>
2023-08-25 17:56:32 +0200

    Update client.lua

--- resources/dust_rumors/client.lua
+++ resources/dust_rumors/client.lua
@@ -1,4 +1,4 @@
-local closestPed = nil
+isEntityIsAPedNonPlayer = true
 
 Citizen.CreateThread(function()
     while true do
@@ -9,16 +9,14 @@ Citizen.CreateThread(function()
         if size > 0 then
             for index = 0, size - 1 do
                 local entity = GetIndexedItemInItemset(index, itemSet) -- Add entity in itemSet
+                local model = GetEntityModel(entity)
 
-                local isEntityIsAPlayer, b = GetPlayerTargetEntity(entity)
-                print(isEntityIsAPlayer)
+                if IsPedHuman(entity) == false or IsPedAPlayer(entity) == true then return end 
 
-                if isEntityIsAPlayer == false then 
-                    local entityPos = GetEntityCoords(entity) 
-                    boneCoord = GetWorldPositionOfEntityBone(entity, 31086)
-                    coords = entityPos + boneCoord
-                    DrawText3D(coords.x, coords.y, coords.z + 1, "MONSTRE")
-                end
+                local entityPos = GetEntityCoords(entity) 
+                boneCoord = GetWorldPositionOfEntityBone(entity, 31086)
+                coords = entityPos + boneCoord
+                DrawText3D(coords.x, coords.y, coords.z + 1, "MONSTRE")
             end
         end
 
