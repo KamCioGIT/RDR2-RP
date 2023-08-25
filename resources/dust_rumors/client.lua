f9d5ccf0f89910c9ce8353a86aac207d418cacef
Aurélien <cordaurelien@gmail.com>
2023-08-25 15:32:27 +0200

    rumors wip

---
+++ resources/dust_rumors/client.lua
@@ -0,0 +1,28 @@
+local closestPed = nil
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(3500)
+        local itemSet = CreateItemset(true)
+        local size = Citizen.InvokeNative(0x59B57C4B06531E1E, GetEntityCoords(PlayerPedId()), 5.0, itemSet, 1, Citizen.ResultAsInteger())
+        -- number xPos, number yPox, number zPos, float distance, int itemSet, int entityType
+
+        print(size)
+
+        if size > 0 then
+            for index = 0, size - 1 do
+                local entity = GetIndexedItemInItemset(index, itemSet)
+                local entityPos = GetEntityCoords(entity)
+
+                if entityPos == GetEntityCoords(PlayerPedId()) then -- Si c'est le joueur
+                    return 
+                end
+            end
+        end
+
+        if IsItemsetValid(itemSet) then
+            DestroyItemset(itemSet)
+        end
+
+    end
+end)
