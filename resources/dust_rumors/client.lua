2ea43473111dcff71f9b9d80ebdddc00f1a72e8a
Aurélien <cordaurelien@gmail.com>
2023-08-25 16:06:29 +0200

    Update client.lua

--- resources/dust_rumors/client.lua
+++ resources/dust_rumors/client.lua
@@ -14,21 +14,34 @@ Citizen.CreateThread(function()
                 local entity = GetIndexedItemInItemset(index, itemSet)
                 local entityPos = GetEntityCoords(entity)
 
-                DrawText3D(coord, "MONSTRE")
+                local id = GetPedIndexFromEntityIndex(entity)
 
-                -- if IsEntityAPed(entity) == true then 
-                   
-                -- end
+                boneCoord = id.GetBoneCoord(Bone.SKEL_Head);
+                ClearDrawOrigin(boneCoord.x, boneCoord.y + 0.5, boneCoord.z)
 
-                -- if entityPos == GetEntityCoords(PlayerPedId()) then -- Si c'est le joueur
-                --     return 
-                -- end
+                DrawText3D(boneCoord.x, boneCoord.y + 0.5, boneCoord.z, "MONSTRE")
             end
         end
 
         if IsItemsetValid(itemSet) then
             DestroyItemset(itemSet)
         end
-
     end
 end)
+
+
+function DrawText3D(x, y, z, text)
+	local onScreen,_x,_y=GetScreenCoordFromWorldCoord(x, y, z)
+	local px,py,pz=table.unpack(GetGameplayCamCoord())  
+	local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+	local str = CreateVarString(10, "LITERAL_STRING", text, Citizen.ResultAsLong())
+	if onScreen then
+		SetTextScale(0.30, 0.30)
+		SetTextFontForCurrentCommand(0)
+		SetTextColor(255, 255, 255, 255)
+		SetTextCentre(1)
+		SetTextDropshadow(1, 1, 0, 0, 200)
+		DisplayText(str,_x,_y)
+		local factor = (string.len(text)) / 225
+	end
+end
