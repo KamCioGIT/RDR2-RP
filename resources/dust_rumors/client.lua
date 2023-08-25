deefebcb2542f5bc7adaf44e7a1238bed5d9c5f9
Aurélien <cordaurelien@gmail.com>
2023-08-25 18:00:19 +0200

    Update client.lua

--- resources/dust_rumors/client.lua
+++ resources/dust_rumors/client.lua
@@ -11,7 +11,7 @@ Citizen.CreateThread(function()
                 local entity = GetIndexedItemInItemset(index, itemSet) -- Add entity in itemSet
                 local model = GetEntityModel(entity)
 
-                if IsPedHuman(entity) == true or IsPedAPlayer(entity) == false then
+                if IsPedHuman(entity) == true and IsPedAPlayer(entity) == false then
                     local entityPos = GetEntityCoords(entity) 
                     boneCoord = GetWorldPositionOfEntityBone(entity, 31086)
                     coords = entityPos + boneCoord
