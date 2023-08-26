a1ca750c30cb8047b9af587ba5c77dafb6635015
levraimurphy <pellouinalexandre@gmail.com>
2023-08-26 14:19:01 +0200

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -460,6 +460,7 @@ function buyhorse()
         
         function(data, menu)
             MenuData.CloseAll()
+            print (data.current.value)
             TriggerServerEvent("redemrp_inventory:createhorse", data.current.value)
         end,
 
