fd2205dd4e70a859b165e1cf3135599ad70e9107
levraimurphy <pellouinalexandre@gmail.com>
2023-08-26 18:02:12 +0200

    Update dust_stable.sql

--- resources/dust_stable/dust_stable.sql
+++ resources/dust_stable/dust_stable.sql
@@ -1,29 +1,16 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
-
 CREATE TABLE IF NOT EXISTS `stable` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `horseid` int(11) NOT NULL,
   `identifier` varchar(40) NOT NULL,
   `charid` int(11) NOT NULL,
   `selected` int(11) NOT NULL DEFAULT 0,
-  `stable` varchar(50) NOT NULL,
-  `model` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `stable` varchar(50) NULL,
+  `model` varchar(50) NULL,
+  `name` varchar(50) NULL,
   `components`  varchar(5000) NOT NULL DEFAULT '{}',
-  `job` varchar(50) NOT NULL,
-  `jobgrade` int(11) NOT NULL,
-  `gang` varchar(50) NOT NULL,
-  `ganggrade` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_horses_characters` (`charid`)
-);
+  `job` varchar(50) NOT NULL DEFAULT 'x',
+  `jobgrade` int(11) NOT NULL DEFAULT 0,
+  `gang` varchar(50) NOT NULL DEFAULT 'x',
+  `ganggrade` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
