923d069845a7c9ffc38523f672d03d3228cd4bbd
levraimurphy <pellouinalexandre@gmail.com>
2023-08-28 15:54:21 +0200

    Update server.lua

--- resources/dust_stable/server.lua
+++ resources/dust_stable/server.lua
@@ -245,7 +245,7 @@ AddEventHandler("RegisterUsableItem:transferhorse", function(source, _data)
 	local user = RedEM.GetPlayer(_source)
 	local identifier = user.identifier
 	local charid = user.charid
-	local ItemData = data.getItem(_source, _data)
+	local ItemData = _data
 	ItemData.RemoveItem(1)
 	MySQL.update('UPDATE stable SET `identifier`=@identifier, `charid`=@charid WHERE `horseid`=@horseid;',
 	{
