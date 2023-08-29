b39536ccbcbf5433cb886fd5bd8710c29e823573
levraimurphy <pellouinalexandre@gmail.com>
2023-08-29 13:59:34 +0200

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -467,7 +467,7 @@ end
 RegisterNetEvent("dust_stable:server:horsestocked")
 AddEventHandler("dust_stable:server:horsestocked", function()
     local horse = GetMount(PlayerPedId())
-    Citizen.InvokeNative(0x5337B721C51883A9, PlayerPedId(), false, false)
+    Citizen.InvokeNative(0x5337B721C51883A9, PlayerPedId(), true, true)
     Wait(1000)
     DeleteEntity(horse)
 end)
