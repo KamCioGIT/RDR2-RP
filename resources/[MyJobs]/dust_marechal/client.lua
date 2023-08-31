a656c1dcf501561a62d1c2e153df2b4a9e802222
levraimurphy <pellouinalexandre@gmail.com>
2023-08-31 15:49:47 +0200

    Update client.lua

--- resources/[MyJobs]/dust_marechal/client.lua
+++ resources/[MyJobs]/dust_marechal/client.lua
@@ -225,9 +225,24 @@ AddEventHandler('rdr_marechal:OpenCustomMenu', function(ClothesComponents, horse
             CompCache[k].texture = 0
         end
     end
-    OldCompCache = deepCopy(CompCache)
+    OldCompCache = deepcopy(CompCache)
     OpenCustomMenu(horse)
 end)
+
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
 
 function Change(id, category, change_type, horse)
     if id < 1 then
