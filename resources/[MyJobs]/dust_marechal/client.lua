e33bc4360b0afb37b720212bc96a6aeeb14f61eb
levraimurphy <pellouinalexandre@gmail.com>
2023-09-01 23:00:37 +0200

    Update client.lua

--- resources/[MyJobs]/dust_marechal/client.lua
+++ resources/[MyJobs]/dust_marechal/client.lua
@@ -164,7 +164,9 @@ function OpenCategory(menu_catagory, horse, horseid)
         menu.close()
         OpenCustomMenu(horse, horseid)
     end, function(data, menu)
-        MenuUpdateComp(data, menu, horse)
+        if data.current.value ~= 0 then 
+            MenuUpdateComp(data, menu, horse)
+        end
     end)
 end
 
