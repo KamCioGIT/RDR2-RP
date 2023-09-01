d2939e4a612ba2e7ad012299a459d1d3c9fec5b6
levraimurphy <pellouinalexandre@gmail.com>
2023-09-01 17:41:38 +0200

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -417,8 +417,9 @@ function spawnhorse(model, name, horseid)
 
     SetPedPromptName(horse, name)
     Entity(horse).state.horseid = horseid
+    print (selectedcomp[k].hash)
     for _, componentHash in pairs(selectedcomp) do
-        print (componentHash.hash)
+        
         Citizen.InvokeNative(0xD3A7B003ED343FD9, horse, componentHash.hash, true, true, true)
     end
 
