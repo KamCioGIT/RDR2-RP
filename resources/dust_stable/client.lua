e1b813f2442e1300499fd2f274fffd4bda701ace
levraimurphy <pellouinalexandre@gmail.com>
2023-09-02 15:00:41 +0200

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -422,9 +422,7 @@ function spawnhorse(model, name, horseid)
 
     --- SET LES META DU CHEVAL
     for k, v in pairs(Config.Label) do
-        if selectedcomp[k].hash ~= 0 then
-            Citizen.InvokeNative(0xD3A7B003ED343FD9, horse, selectedcomp[k].hash, true, true, true)
-        end
+        Citizen.InvokeNative(0xD3A7B003ED343FD9, horse, selectedcomp[k].hash, true, true, true)
     end
     SetPedConfigFlag(horse, 297, true)
     print (meta.health, meta.stamina)
