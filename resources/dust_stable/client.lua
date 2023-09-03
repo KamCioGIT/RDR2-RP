12b85eed7101d5be643cd8d04ca290d38dda0934
levraimurphy <pellouinalexandre@gmail.com>
2023-09-03 04:10:45 +0200

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -821,7 +821,7 @@ AddEventHandler('horse:horsereviver', function(source)
             for index = 0, size - 1 do
                 local entity = GetIndexedItemInItemset(index, itemSet) -- Add entity in itemSet
                 local spawnPosition = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, 0.0)
-                SetEntityCoords(entity, spawnPosition.x, spawnPosition.y, spawnPosition.z, 1, 1, 1, 0)
+                SetEntityCoords(entity, spawnPosition.x, spawnPosition.y, spawnPosition.z, 0, 0, 1, 0)
                 ResurrectPed(entity)
             end
         end
