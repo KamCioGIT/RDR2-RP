7b2a38683065865f35fc5166661c0f4305f33bd3
levraimurphy <pellouinalexandre@gmail.com>
2023-09-05 19:48:38 +0200

    Update config.lua

--- resources/[GUM]/gum_posters/config.lua
+++ resources/[GUM]/gum_posters/config.lua
@@ -92,7 +92,7 @@ Config.Posters = {
         blip=true,
         name="Panneau d'affichage - New Hannover", --- saint denis
         object="mp005_p_mp_bountyboard01x",
-        coords={(2573.404, -782.3102, 42.34581, 250.9799)},
+        coords={2573.404, -782.3102, 42.34581, 250.9799},
     },
 }
 
