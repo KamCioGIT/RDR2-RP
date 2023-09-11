5b628161a406093144d4fe35162d7ed507c2585c
levraimurphy <pellouinalexandre@gmail.com>
2023-09-11 23:20:38 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -291,6 +291,7 @@ function OpenCategoryWeapon(menu_catagory, wepHash, Weapontype, ped)
             end
         elseif  menu_catagory == "specialweapon" then 
             if weapon_comp["model_specific_components"][wepHash][k] ~= nil then
+                print 'meow'
                 category = weapon_comp["model_specific_components"][wepHash][k]
             end
         end
