8abb52f915458be2b354ff223ffc294f2c2d1ed5
levraimurphy <pellouinalexandre@gmail.com>
2023-09-11 19:58:15 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -292,7 +292,9 @@ function OpenCategoryWeapon(menu_catagory, wepHash, Weapontype, ped)
         end
         local options = {}
         for k, v in pairs(category) do
-            table.insert(options, k)
+            if v ~= nil then
+                table.insert(options, k)
+            end
         end
         table.insert(elements, {
             label = Config.LabelW[k] or v,
