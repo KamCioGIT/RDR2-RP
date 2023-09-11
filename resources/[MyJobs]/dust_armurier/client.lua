79d74d1a0c76eac3a61d8247c76fde080b537236
levraimurphy <pellouinalexandre@gmail.com>
2023-09-11 22:04:05 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -284,14 +284,14 @@ function OpenCategoryWeapon(menu_catagory, wepHash, Weapontype, ped)
     local elements = {}
     local a = 1
     for v, k in pairs(Config.MenuElementsW[menu_catagory].category) do
-        if menu_catagory == "special" then 
-            if model_specific_components[wepHash][k] ~= nil then
-                category = model_specific_components[wepHash][k]
-            end
-        end
         if menu_catagory == "commun" then 
             if shared_components[Weapontype][k] ~= nil then
                 category = shared_components[Weapontype][k]
+            end
+        end
+        if menu_catagory == "special" then 
+            if model_specific_components[wepHash][k] ~= nil then
+                category = model_specific_components[wepHash][k]
             end
         end
         local options = {}
