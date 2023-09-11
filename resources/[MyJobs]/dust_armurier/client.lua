4cb9563d2f6a9df2f5366a2003b7d67714a9e9a3
levraimurphy <pellouinalexandre@gmail.com>
2023-09-11 20:51:38 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -350,7 +350,6 @@ function MenuUpdateWeapon(data, menu, wepHash, Weapontype, ped)
         -- CompCache[data.current.category] = weapon_component_model_hash
     end
     RequestModel(model)
-    Citizen.InvokeNative(0x74C9090FDD1BB48E, PlayerPedId(), model, wepHash, true)  -- GiveWeaponComponentToEntity
     if weapon_component_model_hash and weapon_component_model_hash ~= 0 then
         RequestModel(weapon_component_model_hash)
         local i = 0
