5900a95f0fc634f9005396d4e15e254a6246b848
levraimurphy <pellouinalexandre@gmail.com>
2023-09-12 20:54:52 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -322,7 +322,6 @@ function posecoffre(model)
                 -- Citizen.InvokeNative(0x2A32FAA57B937173, -1795314153, playerpos.x, playerpos.y, playerpos.z - 1.0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0)
                 local tempvault = CreateObject(model, playerpos.x, playerpos.y, playerpos.z, false, true, true)
                 SetEntityAlpha(tempvault, 50, false)
-                Wait(0)
                 DeleteEntity(tempvault)
                 if PromptHasHoldModeCompleted(LeavePrompt) then
                     print ("pose")
