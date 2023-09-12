ac3d35948111d1027371767b23d800d1042f5778
levraimurphy <pellouinalexandre@gmail.com>
2023-09-12 20:48:25 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -319,7 +319,9 @@ function posecoffre(model)
                 PromptSetActiveGroupThisFrame(PoseCoffrePromptGroup, PoseCoffrePromptName)
                  ---- Lancer anim porter une caisse
                 local playerpos = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.5, 0)
-                Citizen.InvokeNative(0x2A32FAA57B937173, -1795314153, playerpos.x, playerpos.y, playerpos.z - 1.0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0)
+                -- Citizen.InvokeNative(0x2A32FAA57B937173, -1795314153, playerpos.x, playerpos.y, playerpos.z - 1.0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0)
+                local tempvault = CreateObject(model, playerpos.x, playerpos.y, playerpos.z, false, true, true)
+                SetEntityAlpha(tempvault, 50)
                 if PromptHasHoldModeCompleted(LeavePrompt) then
                     print ("pose")
                     ---- cancel anim
