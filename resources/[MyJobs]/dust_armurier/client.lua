a62b404d7fb853d681a049cc7ee636d619741516
levraimurphy <pellouinalexandre@gmail.com>
2023-09-12 16:15:40 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -454,6 +454,7 @@ RegisterNetEvent("dust_armurier:getuid", function(name, uid, comp, WeapType, ped
     wep_name = name
     wep_uid = uid
     NewCompCache = comp
+    print (wep_name, type(comp))
     local hashwep = GetHashKey(wep_name)
     for k, v in pairs(weapon_comp["model_specific_components"]) do
         if k == hashwep then
