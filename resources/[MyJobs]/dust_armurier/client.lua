863badce314286acf285c67488ee55e185f80174
levraimurphy <pellouinalexandre@gmail.com>
2023-09-12 20:20:38 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -419,7 +419,15 @@ RegisterNetEvent("dust_armurier:getuid", function(name, uid, comp, WeapType, ped
     wep_name = name
     wep_uid = uid
     NewCompCache = comp
+    _wephash = GetHashKey(name)
     for k, v in pairs(weapon_comp["shared_components"][WeapType]) do
+        if NewCompCache[k] == nil then
+            NewCompCache[k] = {}
+            NewCompCache[k] = 0
+        end
+        print (NewCompCache[k])
+    end
+    for k, v in pairs(weapon_comp["model_specific_components"][_wephash]) do
         if NewCompCache[k] == nil then
             NewCompCache[k] = {}
             NewCompCache[k] = 0
