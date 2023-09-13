bf01ac918c889a4b1ccd92cee8028ecbb7ebea80
levraimurphy <pellouinalexandre@gmail.com>
2023-09-13 11:39:02 +0200

    Update client.lua

--- resources/dust_vault/client.lua
+++ resources/dust_vault/client.lua
@@ -313,16 +313,16 @@ function posecoffre(model)
     PoseCoffrePromptShown = false
     Citizen.CreateThread(function()
         while not spawned do
-            Citizen.Wait(0)
+            Citizen.Wait(1)
             local playerPed = PlayerPedId()
             if PoseCoffrePromptShown == false then
                 PromptSetActiveGroupThisFrame(PoseCoffrePromptGroup, PoseCoffrePromptName)
                  ---- Lancer anim porter une caisse
                 local playerpos = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.5, 0)
                 Citizen.InvokeNative(0x2A32FAA57B937173, -1795314153, playerpos.x, playerpos.y, playerpos.z - 1.0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0)
-                -- local tempvault = CreateObject(model, playerpos.x, playerpos.y, playerpos.z, false, true, true)
-                -- SetEntityAlpha(tempvault, 50, false)
-                -- DeleteEntity(tempvault)
+                local tempvault = CreateObject(model, playerpos.x, playerpos.y, playerpos.z, false, true, true)
+                SetEntityAlpha(tempvault, 50, false)
+                DeleteEntity(tempvault)
                 if PromptHasHoldModeCompleted(LeavePrompt) then
                     ---- cancel anim
                     PoseCoffrePromptShown = true
