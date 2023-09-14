6b2b36b8611820032c03e44a62d813669a2bcaf1
levraimurphy <pellouinalexandre@gmail.com>
2023-09-14 18:36:17 +0200

    Update config.lua

--- resources/dust_vault/config.lua
+++ resources/dust_vault/config.lua
@@ -26,9 +26,9 @@ Config.IdleAnim = {'idle_b'}
 
 Config.WorkingTime = 3000
 
-Config.SmallVault = GetHashKey("p_safe_ser") ---- 1408938498
-Config.MediumVault = GetHashKey("s_safe03x") --- 230276839
-Config.LargeVault = GetHashKey("p_safe_01")---- Ancien -1212348817
+Config.SmallVault = 3797250019 -- GetHashKey("p_safe_ser") ---- 1408938498
+Config.MediumVault = 442299187 -- GetHashKey("s_safe03x") --- 230276839
+Config.LargeVault = 575786725 -- GetHashKey("p_safe_01")---- Ancien -1212348817
 
 Config.DistanceAffichage = 100.0
 
