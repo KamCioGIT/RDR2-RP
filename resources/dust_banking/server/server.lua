ef6e5e6359af49cc583105181b4e1a9e6dd780c6
levraimurphy <pellouinalexandre@gmail.com>
2023-09-18 14:21:07 +0200

    Update server.lua

--- resources/dust_banking/server/server.lua
+++ resources/dust_banking/server/server.lua
@@ -144,7 +144,7 @@ RedEM.RegisterCallback('qbr-banking:getBankingInformation', function(source, cb)
                     ['name'] = xPlayer.firstname .. ' ' .. xPlayer.lastname,
                     ['bankbalance'] = '$'.. format_int(xPlayer.bankmoney),
                     ['cash'] = '$'.. format_int(xPlayer.money),
-                    ['accountinfo'] = XPlayer.pobox,
+                    ['accountinfo'] = 'N°' ..tostring(XPlayer.pobox),
                 }
                 --[[
                 if savingsAccounts[xPlayer.PlayerData.citizenid] then
