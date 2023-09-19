c31ca2ba4b25c6089281e25f387ef5f0d8d32d0a
levraimurphy <pellouinalexandre@gmail.com>
2023-09-19 17:47:28 +0200

    fix

--- resources/dust_banking/client/client.lua
+++ resources/dust_banking/client/client.lua
@@ -51,7 +51,6 @@ AddEventHandler('qbr-banking:openBusinessScreen', function()
 end)
 function openBusinessScreen()
     RedEM.TriggerCallback('qbr-banking:getBusinessInformation', function(businessinfo)
-        print (businessinfo)
         if businessinfo ~= nil then
             InBank = true
             SetNuiFocus(true, true)
