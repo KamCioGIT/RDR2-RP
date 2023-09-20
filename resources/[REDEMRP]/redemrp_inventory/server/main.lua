27e6cf2a37b58897c0a1a7bd9b221124c90bfb5d
levraimurphy <pellouinalexandre@gmail.com>
2023-09-20 21:32:50 +0200

    Update main.lua

--- resources/[REDEMRP]/redemrp_inventory/server/main.lua
+++ resources/[REDEMRP]/redemrp_inventory/server/main.lua
@@ -2225,27 +2225,24 @@ end
 exports("checkuserweight", checkuserweight)
 
 RegisterServerEvent("redemrp_inventory:ChangeWaterAmmount")
-AddEventHandler(
-    "redemrp_inventory:ChangeWaterAmmount",
-    function(water, meta, type)
-        local _source = source
-        local _table = table
-        local Player = RedEM.GetPlayer(_source)
-        if Player then
-            local identifier = Player.GetIdentifier()
-            local charid = Player.GetActiveCharacter()
-            local player_inventory = Inventory[identifier .. "_" .. charid]
-            local item, id = getInventoryItemFromName("gourde", player_inventory, meta)
-            if type == "remplir" then
-                if item then
-                    item.setMeta({water = 100})
-                end
-            else if type == "boire" then
-                newwater = water - 20
-                if item then
-                    item.setMeta({water = newwater})
-                end
+AddEventHandler("redemrp_inventory:ChangeWaterAmmount", function(water, meta, type)
+    local _source = source
+    local _table = table
+    local Player = RedEM.GetPlayer(_source)
+    if Player then
+        local identifier = Player.GetIdentifier()
+        local charid = Player.GetActiveCharacter()
+        local player_inventory = Inventory[identifier .. "_" .. charid]
+        local item, id = getInventoryItemFromName("gourde", player_inventory, meta)
+        if type == "remplir" then
+            if item then
+                item.setMeta({water = 100})
+            end
+        elseif type == "boire" then
+            newwater = water - 20
+            if item then
+                item.setMeta({water = newwater})
             end
         end
     end
-)
+end)
