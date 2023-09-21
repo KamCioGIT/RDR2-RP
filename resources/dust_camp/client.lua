8a0c9c2a601b397de185c4f5611ff8db196d99f6
levraimurphy <pellouinalexandre@gmail.com>
2023-09-21 23:28:43 +0200

    Update client.lua

--- resources/dust_camp/client.lua
+++ resources/dust_camp/client.lua
@@ -8,7 +8,7 @@ local spawncauldron= false
 local campprompt = UipromptGroup:new("Feu de camp")
 Uiprompt:new(0x5181713D, "Cuisiner", campprompt)
 Uiprompt:new(0x8E90C7BB, "Démonter", campprompt):setHoldMode(true)
-Uiprompt:new(0x9959A6F0, "Purifier l'eau de la gourde", campprompt)
+Uiprompt:new(0x9959A6F0, "Purifier l'eau", campprompt)
 campprompt:setActive(false)
 
 Citizen.CreateThread(function()
