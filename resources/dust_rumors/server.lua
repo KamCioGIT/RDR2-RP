d890073552645a9505d732c4b99442f0c920b8ed
Aurélien <71558277+Arcaane@users.noreply.github.com>
2023-09-21 14:49:09 +0200

    Update server.lua

--- resources/dust_rumors/server.lua
+++
@@ -1,7 +0,0 @@
-RedEM = exports["redem_roleplay"]:RedEM()
-
-RegisterServerEvent('rumors:checkplayer')
-AddEventHandler('rumors:checkplayer', function(index)
-    print(Citizen.InvokeNative(0x404794CA, entity)) 
-    return Citizen.InvokeNative(0x404794CA, entity)
-end)
