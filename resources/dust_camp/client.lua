00661b11a5309a67c6ec64b7e5ace82ced242ff2
levraimurphy <pellouinalexandre@gmail.com>
2023-09-21 23:27:03 +0200

    Update client.lua

--- resources/dust_camp/client.lua
+++ resources/dust_camp/client.lua
@@ -91,6 +91,28 @@ Citizen.CreateThread(function()
             local objectPos = GetEntityCoords(cookgrill)
             if  #(pos - objectPos) < 1.5 and not isInteracting then
                 campprompt:setActiveThisFrame(true)
+                if IsControlJustReleased(0, 0x9959A6F0) then
+                    isInteracting = true
+                    local playerPed = PlayerPedId()
+                    RequestAnimDict(Config.MenuDict)
+                    while not HasAnimDictLoaded(Config.MenuDict) do
+                        Citizen.Wait(50)
+                    end
+                    for k,v in pairs(Config.MenuAnim) do
+                        TaskPlayAnim(playerPed, Config.MenuDict, v, 8.0, -8.0, -1, 2, 0, true)
+                    end
+                    Citizen.Wait(5000)
+                    RequestAnimDict(Config.CloseMenuDict)
+                    while not HasAnimDictLoaded(Config.CloseMenuDict) do
+                        Citizen.Wait(50)
+                    end
+                    for k,v in pairs(Config.CloseMenuAnim) do
+                        TaskPlayAnim(playerPed, Config.CloseMenuDict, v, 8.0, -8.0, -1, 0, 0, true)
+                        Citizen.Wait(1000)
+                    end
+                    TriggerServerEvent("redemrp_inventory:ChangeWaterAmmount", "purifier")
+                    isInteracting = false
+                end
                 if IsControlJustReleased(0, 0x5181713D) then
                     isInteracting = true
                     local playerPed = PlayerPedId()
@@ -137,6 +159,28 @@ Citizen.CreateThread(function()
             local objectPos = GetEntityCoords(cauldron)
             if #(pos - objectPos) < 1.5 and not isInteracting then
                 campprompt:setActiveThisFrame(true)
+                if IsControlJustReleased(0, 0x9959A6F0) then
+                    isInteracting = true
+                    local playerPed = PlayerPedId()
+                    RequestAnimDict(Config.MenuDict)
+                    while not HasAnimDictLoaded(Config.MenuDict) do
+                        Citizen.Wait(50)
+                    end
+                    for k,v in pairs(Config.MenuAnim) do
+                        TaskPlayAnim(playerPed, Config.MenuDict, v, 8.0, -8.0, -1, 2, 0, true)
+                    end
+                    Citizen.Wait(5000)
+                    RequestAnimDict(Config.CloseMenuDict)
+                    while not HasAnimDictLoaded(Config.CloseMenuDict) do
+                        Citizen.Wait(50)
+                    end
+                    for k,v in pairs(Config.CloseMenuAnim) do
+                        TaskPlayAnim(playerPed, Config.CloseMenuDict, v, 8.0, -8.0, -1, 0, 0, true)
+                        Citizen.Wait(1000)
+                    end
+                    TriggerServerEvent("redemrp_inventory:ChangeWaterAmmount", "purifier")
+                    isInteracting = false
+                end
                 if IsControlJustReleased(0, 0x5181713D) then
                     isInteracting = true
                     local playerPed = PlayerPedId()
