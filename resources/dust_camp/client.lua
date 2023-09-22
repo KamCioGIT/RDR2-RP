c968a465b0d08a7ce5a816ed3f5fb2f3b77b92cd
levraimurphy <pellouinalexandre@gmail.com>
2023-09-22 18:46:19 +0200

    fix

--- resources/dust_camp/client.lua
+++ resources/dust_camp/client.lua
@@ -263,7 +263,9 @@ RegisterNetEvent("camp:OpenCampMenu", function(menutype)
             table.insert(elements, {label = "Gros Steak au Thym", value = 'gunpowder', desc = "Recette: 1 Steak + 1 Thym"})
         end
         if _menutype == 'cauldron' then 
-            table.insert(elements, {label = "Gros Steak et Carotte Sauvage", value = 'grossteakcarottesauvage', descriptionimages = {src = 'nui://redemrp_inventory/html/items/provision_meat_prime_beef.png', text = "Gros Steak",count = "x1"}, {src = 'nui://redemrp_inventory/html/items/consumable_herb_wild_carrots.png', text = "Carotte Sauvage",count = "x2"}})
+            table.insert(elements, {label = "Gros Steak et Carotte Sauvage", value = 'grossteakcarottesauvage', descriptionimages = 
+            {{src = 'nui://redemrp_inventory/html/items/provision_meat_prime_beef.png', text = "Gros Steak",count = "x1"}, 
+            {src = 'nui://redemrp_inventory/html/items/consumable_herb_wild_carrots.png', text = "Carotte Sauvage",count = "x2"}}})
         end
 
         MenuData.Open('default', GetCurrentResourceName(), 'Camp', {
