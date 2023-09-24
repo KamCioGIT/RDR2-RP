ecfba6e7d46d1e1f50afa7b848c18d456eb693ea
levraimurphy <pellouinalexandre@gmail.com>
2023-09-24 22:37:22 +0200

    fix

--- resources/redem_roleplay/client/cl_notification.lua
+++ resources/redem_roleplay/client/cl_notification.lua
@@ -303,3 +303,5 @@ Citizen.InvokeNative(0xCB5D11F9508A928D, 1, a2:Buffer(), a3:Buffer(), GetHashKey
 local a2 = DataView.ArrayBuffer(12 * 8)
 local a3 = DataView.ArrayBuffer(12 * 8)
 Citizen.InvokeNative(0xCB5D11F9508A928D, 1, a2:Buffer(), a3:Buffer(), GetHashKey("UPGRADE_STAMINA_TANK_1"), 1084182731, Config.MaxStaminaCore, 752097756)
+
+SetPedHeadshotDamageMultiplier(playerPedId(), 0.3)
