e19dd255a2251f48909cb907e6591a9eacc9c51e
levraimurphy <pellouinalexandre@gmail.com>
2023-09-26 00:06:54 +0200

    Update qbr-banking.js

--- resources/dust_banking/nui/qbr-banking.js
+++ resources/dust_banking/nui/qbr-banking.js
@@ -276,7 +276,7 @@ function populateBanking(data)
         $('#currentStatement').DataTable({
             "order": [[ 0, "desc" ]],
             "pagingType": "simple",
-            "lengthMenu": [[20, 35, 50, -1], [20, 35, 50, "All"]]
+            "lengthMenu": [[20, 35, 50, -1], [20, 35, 50, "All"]],
             "language": {"url": "nui\fr-FR.json"}
         });
     } );
