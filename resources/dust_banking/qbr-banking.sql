87aab61ebd7a87ebc830ed651bb781d77ede3dd2
levraimurphy <pellouinalexandre@gmail.com>
2023-09-25 19:11:29 +0200

    Update qbr-banking.sql

--- resources/dust_banking/qbr-banking.sql
+++ resources/dust_banking/qbr-banking.sql
@@ -6,10 +6,9 @@ CREATE TABLE IF NOT EXISTS `bank_accounts` (
   `balance` bigint(255) NOT NULL DEFAULT 0,
   `account_type` enum('Savings','Business') NOT NULL DEFAULT 'Savings',
   PRIMARY KEY (`record_id`),
-  UNIQUE KEY `citizenid` (`citizenid`),
   UNIQUE KEY `accountid` (`accountid`),
   KEY `job` (`job`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `bank_statements` (
   `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
@@ -23,4 +22,4 @@ CREATE TABLE IF NOT EXISTS `bank_statements` (
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`record_id`),
   KEY `job` (`job`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
