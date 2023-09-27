27a1f202bd563381ec01a0af053855c2b9db1a0c
levraimurphy <pellouinalexandre@gmail.com>
2023-09-27 17:08:12 +0200

    trad

--- resources/mega_doctorjob/ui/js/config.js
+++ resources/mega_doctorjob/ui/js/config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     // Translation for boneIDs to readable names
     boneNames: {
-        'head': 'Head',
-        'right-arm': 'Right Arm',
-        'left-arm': 'Left Arm',
-        'right-leg': 'Right Leg',
-        'left-leg': 'Left Leg',
-        'torso': 'Torso'
+        'head': 'Tête',
+        'right-arm': 'Bras Droit',
+        'left-arm': 'Bras Gauche',
+        'right-leg': 'Jambe Droite',
+        'left-leg': 'Jambe Gauche',
+        'torso': 'Torse'
     },
     defaultBodyTemperatureRange: [36.0, 37.5],
     defaultHeartRate: 'Normal',
     language: {
-        cut: 'Cut',
+        cut: 'Plaie',
         projectile: 'Projectile',
-        broken: 'Broken',
-        bandaged: 'Dirty Bandage'
+        broken: 'Fracture',
+        bandaged: 'Bandage usé'
     },
     // Don't touch unless you know what you're doing
     // Used for the medical ui to localise damage, it links to the svg file ids
