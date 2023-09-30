06fa28dc759dde50dd4bcc1ba37f5986f03d98fc
levraimurphy <pellouinalexandre@gmail.com>
2023-09-30 21:39:24 +0200

    add forge inshallah ça passe

---
+++ resources/[MAPPING]/dust_forge/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+files {
+    'stream/mlo_bla_blacksmith.ytyp'
+}
+
+lua54 "yes"
+
+this_is_a_map "yes"
+data_file 'DLC_ITYP_REQUEST' 'stream/mlo_bla_blacksmith.ytyp'
+dependency '/assetpacks'
