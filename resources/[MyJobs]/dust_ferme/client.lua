94102b7ae94089abbd932445e2116fb0f95c7b9a
levraimurphy <pellouinalexandre@gmail.com>
2023-10-01 23:43:57 +0200

    Update client.lua

--- resources/[MyJobs]/dust_ferme/client.lua
+++ resources/[MyJobs]/dust_ferme/client.lua
@@ -581,7 +581,7 @@ Citizen.CreateThread(function ()
                                     while not HasAnimDictLoaded(Config.PedMilkingDict) do
                                         Wait(10)
                                     end
-                                    TaskPlayAnim(PlayerPedId(), Config.PedMilkingDict, Config.PedMilkingAnim, 1.0, 1.0, -1, -1, 0, false, false, false)
+                                    TaskPlayAnim(PlayerPedId(), Config.PedMilkingDict, Config.PedMilkingAnim, 1.0, 1.0, -1, 1, 0, false, false, false)
                                     Wait(5000)
                                     ClearPedTasks(PlayerPedId())
                                     TriggerServerEvent("dust_ferme:serveur:milking", Entity(entity).state.cowid)
