dc1e9d5084763bc860629d432abfc14611c15ede
levraimurphy <pellouinalexandre@gmail.com>
2023-10-01 23:14:55 +0200

    Update server.lua

--- resources/[MyJobs]/dust_ferme/server.lua
+++ resources/[MyJobs]/dust_ferme/server.lua
@@ -207,7 +207,7 @@ RegisterServerEvent('dust_ferme:cowup', function(cowid)
 				local cd = os.time()
 				local level = result[i].level
 				local savedDate = result[i].date / 1000 -- Remplacez ceci par la date de votre base de données
-				local timeDifference = os.difftime(savedDate, cd)
+				local timeDifference = os.difftime(cd, savedDate)
 				print (timeDifference)
 				if timeDifference >= 86400 or lastup == nil then
 					if level < 5 then
