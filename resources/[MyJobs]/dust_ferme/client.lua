da87461d6b401480f5a41f366b2aaa8f71c6fb05
levraimurphy <pellouinalexandre@gmail.com>
2023-10-02 18:22:03 +0200

    Update client.lua

--- resources/[MyJobs]/dust_ferme/client.lua
+++ resources/[MyJobs]/dust_ferme/client.lua
@@ -555,7 +555,7 @@ Citizen.CreateThread(function ()
                     if IsControlJustReleased(0, 0x760A9C6F) and Entity(entity).state.grazing ~= true then
                             print('guider')
                             ClearPedTasks(entity)
-                            local duration = math.random(30000, 120000)
+                            local duration = math.random(60000, 120000)
                             TaskGoToEntity(entity, PlayerPedId(), duration, 0.2, 2.0, 0, 0)
                             -- TaskFollowToOffsetOfEntity(entity, PlayerPedId(), 0.0, -3.0, 0.0, 1.0, duration, 100, 1, 1, 0, 0, 1)
                         -- guider
