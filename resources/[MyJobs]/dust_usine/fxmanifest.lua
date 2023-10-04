cf2ccbfc91f5ba3c8f4e730ccbd25bf4cede9d82
levraimurphy <pellouinalexandre@gmail.com>
2023-10-04 18:22:32 +0200

    Update fxmanifest.lua

--- resources/[MyJobs]/dust_usine/fxmanifest.lua
+++ resources/[MyJobs]/dust_usine/fxmanifest.lua
@@ -5,6 +5,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 shared_script 'config.lua'
 
 client_scripts {
+	"@uiprompt/uiprompt.lua",
 	'config.lua',
 	'client.lua',
 }
