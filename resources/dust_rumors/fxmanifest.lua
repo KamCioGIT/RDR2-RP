3614ce0197270fb10e3157ac6f8099b2bdfd2dff
Aurélien <cordaurelien@gmail.com>
2023-10-06 16:58:53 +0200

    Update fxmanifest.lua

--- resources/dust_rumors/fxmanifest.lua
+++ resources/dust_rumors/fxmanifest.lua
@@ -2,12 +2,13 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+shared_script 'cfg.lua'
+
 client_scripts {
-	"@uiprompt/uiprompt.lua",
+	'@uiprompt/uiprompt.lua',
 	'@ox_lib/init.lua',
 	'cfg.lua',
 	'client.lua',
-	'@mysql-async/lib/MySQL.lua'
 }
 
 server_scripts {
