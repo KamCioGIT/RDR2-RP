187b6972f914dbe2007ebbdd3d8d576517b5489d
levraimurphy <pellouinalexandre@gmail.com>
2023-10-06 12:26:46 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -355,9 +355,9 @@ end
 
 function MenuUpdateWeapon(data, menu, wepHash, Weapontype, ped, menu_catagory)    
     if menu_catagory == "specialweapon" then
-        for _, comp in pairs(weapon_comp["model_specific_components"][wepHash][data.current.category]) do
-            Citizen.InvokeNative(0x19F70C4D80494FF8, ped, GetHashKey(comp), wepHash)
-        end
+        -- for _, comp in pairs(weapon_comp["model_specific_components"][wepHash][data.current.category]) do
+        --     Citizen.InvokeNative(0x19F70C4D80494FF8, ped, GetHashKey(comp), wepHash)
+        -- end
 
         
         model = GetHashKey(weapon_comp["model_specific_components"][wepHash][data.current.category][data.current.value])
@@ -365,9 +365,9 @@ function MenuUpdateWeapon(data, menu, wepHash, Weapontype, ped, menu_catagory)
         Citizen.InvokeNative(0x74C9090FDD1BB48E, ped, model, wepHash, true)
         NewCompCache[data.current.category] = model
     else
-        for _, comp in pairs(weapon_comp["shared_components"][Weapontype][data.current.category]) do
-            Citizen.InvokeNative(0x19F70C4D80494FF8, ped, GetHashKey(comp), wepHash)
-        end
+        -- for _, comp in pairs(weapon_comp["shared_components"][Weapontype][data.current.category]) do
+        --     Citizen.InvokeNative(0x19F70C4D80494FF8, ped, GetHashKey(comp), wepHash)
+        -- end
         model = GetHashKey(weapon_comp["shared_components"][Weapontype][data.current.category][data.current.value])
         -- weapon_component_model_hash =  Citizen.InvokeNative(0x59DE03442B6C9598, model)
         Citizen.InvokeNative(0x74C9090FDD1BB48E, ped, model, wepHash, true)
