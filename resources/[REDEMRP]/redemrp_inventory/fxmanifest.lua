7d7d7c0826fc322e3a5d2969026acffda139d006
levraimurphy <pellouinalexandre@gmail.com>
2023-10-06 13:01:57 +0200

    Update fxmanifest.lua

--- resources/[REDEMRP]/redemrp_inventory/fxmanifest.lua
+++ resources/[REDEMRP]/redemrp_inventory/fxmanifest.lua
@@ -2,12 +2,14 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 client_scripts {
+	'wepsharedcomp.lua',
 	'client/dataview.lua',
 	'config.lua',
 	'client/main.lua',
 }
 
 server_scripts {
+	'wepsharedcomp.lua',
 	'config.lua',
 	'@oxmysql/lib/MySQL.lua',
 	'server/items.lua',
