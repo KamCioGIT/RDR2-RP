341a5517f3722ef3e0fd0d69d9c8071cda7f1d51
levraimurphy <pellouinalexandre@gmail.com>
2023-10-08 19:19:39 +0200

    Update client.lua

--- resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
@@ -355,6 +355,7 @@ AddEventHandler('rdr_clothes_store:OpenClothingMenu', function(ClothesComponents
 end)
 
 function Change(id, category, change_type, is_mp)
+    print (is_mp)
     if is_mp == true then
         if id < 1 then
             Citizen.InvokeNative(0xD710A5007C2AC539, PlayerPedId(), GetHashKey(category), 0)
