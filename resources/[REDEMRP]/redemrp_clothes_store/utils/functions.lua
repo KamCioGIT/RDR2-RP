bef9b32ae549a465613b14bebcdf437dd44a1b23
levraimurphy <pellouinalexandre@gmail.com>
2023-10-08 20:16:59 +0200

    Update functions.lua

--- resources/[REDEMRP]/redemrp_clothes_store/utils/functions.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/utils/functions.lua
@@ -122,9 +122,9 @@ function UpdateCustomClothes(playerPed, drawable, albedo, normal, material, pale
     local _normal = normal
     local _material = material
     local _palette = palette
-    local _tint0 = tint0
-    local _tint1 = tint1
-    local _tint2 = tint2
+    local _tint0 = tonumber(tint0)
+    local _tint1 = tonumber(tint1)
+    local _tint2 = tonumber(tint2)
     Wait(100)
     SetMetaPedTag(playerPed, _drawable, _albedo, _normal, _material, _palette, _tint0, _tint1, _tint2)
     UpdatePedVariation(playerPed)
