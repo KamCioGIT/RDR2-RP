a66e8b9d56ce4c2e6258933be7edd665a30d54b9
levraimurphy <pellouinalexandre@gmail.com>
2023-10-08 20:29:18 +0200

    Update client.lua

--- resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
@@ -202,6 +202,19 @@ function MenuUpdateClothes(data, menu)
         if ClothesCache[data.current.category].model ~= data.current.value then
             ClothesCache[data.current.category].texture = 1
             ClothesCache[data.current.category].model = data.current.value
+            if IsPedMale(PlayerPedId()) then
+                if clothes_list["male"][data.current.category][data.current.value][1]['is_multiplayer'] == false then
+                    ClothesCache[data.current.category].ismp = false
+                else
+                    ClothesCache[data.current.category].ismp = true
+                end
+            else
+                if clothes_list["female"][data.current.category][data.current.value][1]['is_multiplayer'] == false then
+                    ClothesCache[data.current.category].ismp = false
+                else
+                    ClothesCache[data.current.category].ismp = true
+                end
+            end
             if data.current.value > 0 then
                 local options = {}
                 -- print(GetMaxTexturesForModel(data.current.category, data.current.value))
@@ -318,6 +331,7 @@ AddEventHandler('rdr_clothes_store:OpenClothingMenu', function(ClothesComponents
                 ClothesCache[k] = {}
                 ClothesCache[k].model = 0
                 ClothesCache[k].texture = 0
+                ClothesCache[k].ismp = true
             end
         end
     else
@@ -326,6 +340,7 @@ AddEventHandler('rdr_clothes_store:OpenClothingMenu', function(ClothesComponents
                 ClothesCache[k] = {}
                 ClothesCache[k].model = 0
                 ClothesCache[k].texture = 0
+                ClothesCache[k].ismp = true
             end
         end
 
@@ -368,11 +383,37 @@ function Change(id, category, change_type)
     else
         if IsPedMale(PlayerPedId()) then
             if change_type == "model" then
+                if clothes_list["male"][category][id][1]['is_multiplayer'] == false then
+                    local drawable = clothes_list["male"][category][id][1].drawable
+                    local albedo = clothes_list["male"][category][id][1].albedo
+                    local normal = clothes_list["male"][category][id][1].normal
+                    local material = clothes_list["male"][category][id][1].material
+                    local palette = clothes_list["male"][category][id][1].palette
+                    local tint0 = clothes_list["male"][category][id][1].tint0
+                    local tint1 = clothes_list["male"][category][id][1].tint1
+                    local tint2 = clothes_list["male"][category][id][1].tint2
+                    print (drawable, albedo, normal, material, palette, tint0, tint1, tint2)
+                    UpdateCustomClothes(PlayerPedId(), drawable, albedo, normal, material, palette, tint0, tint1, tint2)
+                else
                 NativeSetPedComponentEnabled(PlayerPedId(), clothes_list["male"][category][id][1].hash, false, true,
                     true)
+                end
             else
+                if clothes_list["male"][category][id][1]['is_multiplayer'] == false then
+                    local drawable = clothes_list["male"][category][id][1].drawable
+                    local albedo = clothes_list["male"][category][id][1].albedo
+                    local normal = clothes_list["male"][category][id][1].normal
+                    local material = clothes_list["male"][category][id][1].material
+                    local palette = clothes_list["male"][category][id][1].palette
+                    local tint0 = clothes_list["male"][category][id][1].tint0
+                    local tint1 = clothes_list["male"][category][id][1].tint1
+                    local tint2 = clothes_list["male"][category][id][1].tint2
+                    print (drawable, albedo, normal, material, palette, tint0, tint1, tint2)
+                    UpdateCustomClothes(PlayerPedId(), drawable, albedo, normal, material, palette, tint0, tint1, tint2)
+                else
                 NativeSetPedComponentEnabled(PlayerPedId(),
                     clothes_list["male"][category][ClothesCache[category].model][id].hash, false, true, true)
+                end
             end
 
         else
@@ -393,6 +434,18 @@ function Change(id, category, change_type)
                         true)
                 end
             else
+                if clothes_list["female"][category][id][1]['is_multiplayer'] == false then
+                    local drawable = clothes_list["female"][category][id][1].drawable
+                    local albedo = clothes_list["female"][category][id][1].albedo
+                    local normal = clothes_list["female"][category][id][1].normal
+                    local material = clothes_list["female"][category][id][1].material
+                    local palette = clothes_list["female"][category][id][1].palette
+                    local tint0 = clothes_list["female"][category][id][1].tint0
+                    local tint1 = clothes_list["female"][category][id][1].tint1
+                    local tint2 = clothes_list["female"][category][id][1].tint2
+                    print (drawable, albedo, normal, material, palette, tint0, tint1, tint2)
+                    UpdateCustomClothes(PlayerPedId(), drawable, albedo, normal, material, palette, tint0, tint1, tint2)
+                else
                 NativeSetPedComponentEnabled(PlayerPedId(),
                     clothes_list["female"][category][ClothesCache[category].model][id].hash, false, true, true)
             end
