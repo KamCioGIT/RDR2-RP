c1cbdf261702a46ea4637e9f093f09157a0c0955
levraimurphy <pellouinalexandre@gmail.com>
2023-10-09 13:02:14 +0200

    Update telegrams.sql

--- resources/dust_telegram/telegrams.sql
+++ resources/dust_telegram/telegrams.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `telegrams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sender` varchar(50) NOT NULL DEFAULT '0',
-  `recipient` varchar(50) NOT NULL,
+  `sender` int(11) NOT NULL DEFAULT 0,
+  `recipient` int(11) NOT NULL DEFAULT 0,
   `message` longtext NOT NULL,
   `date` datetime NULL,
   `selected` int(11) NOT NULL DEFAULT 0,
