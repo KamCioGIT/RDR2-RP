69b7e43830a7cbac38eca398edcde8f65f358481
levraimurphy <pellouinalexandre@gmail.com>
2023-10-15 01:08:20 +0200

    Update config.lua

--- resources/[MyJobs]/dust_armurier/config.lua
+++ resources/[MyJobs]/dust_armurier/config.lua
@@ -223,9 +223,9 @@ Config.DamageModifier = {
     -- [1701864918]                        = 0.5,          -- weapon_pistol_semiauto
     -- [2239809086]                        = 0.5,          -- weapon_pistol_mauser
 
-    [1905553950]                        = 0.6,          -- weapon_repeater_evans = evans 12 balles
-    [-1783478894]                        = 0.8,          -- weapon_repeater_henry = litchfield 8 balles
-    [-1471716628]                        = 0.8,          -- weapon_repeater_winchester = lancaster 8 balles
+    [1905553950]                        = 0.5,          -- weapon_repeater_evans = evans 12 balles
+    [-1783478894]                        = 0.8,          -- weapon_repeater_henry = litchfield 7 balles
+    [-1471716628]                        = 0.70,          -- weapon_repeater_winchester = lancaster 8 balles
     [-183018591]                        = 0.8,          -- weapon_repeater_carbine = carbine 8 balles
 
     [127400949]                         = 1.2,          -- weapon_revolver_doubleaction 9 balles
