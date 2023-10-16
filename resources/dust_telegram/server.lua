f230e5452a0e972a9d2318a329b2c7a2b3671131
levraimurphy <pellouinalexandre@gmail.com>
2023-10-16 22:45:43 +0200

    Update server.lua

--- resources/dust_telegram/server.lua
+++ resources/dust_telegram/server.lua
@@ -13,14 +13,11 @@ AddEventHandler("scf_telegram:check_inbox", function()
     local firstname = User.firstname
     local lastname = User.lastname
 	local postbox = firstname .. " " .. lastname
-    MySQL.query("SELECT * FROM telegrams WHERE recipient = @reci ORDER BY id DESC", { ['@reci'] = postbox }, function(result)
+    MySQL.query("SELECT * FROM telegrams WHERE `recipient` = @reci", { reci = postbox }, function(result)
         res = {}
-        res['box'] = postbox
-        res['firstname'] = firstname
-        res['list'] = result
-        if result ~= nil then
+        if #result ~= 0 then
+            table.insert(res, {['box'] = postbox, ['firstname'] = firstname, ['list'] = result })
             TriggerClientEvent("inboxlist", _source, res)
-            res = {}
         end
     end)
 end)
