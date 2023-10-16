8e354285817fcaa774d00be5bd2eca22b13f68ce
levraimurphy <pellouinalexandre@gmail.com>
2023-10-16 11:05:32 +0200

    Update config.lua

--- resources/[REDEMRP]/redemrp_creator/utils/config.lua
+++ resources/[REDEMRP]/redemrp_creator/utils/config.lua
@@ -2,6 +2,8 @@ Config = {}
 
 Config.Barber = {
     ['Coiffeur de Saint-Denis'] = vector3(2654.633, -1181.56, 52.27853),
+    ['Coiffeur de Blackwater'] = vector3(-816.0061, -1367.731, 43.75092),
+
 }
 Config.DistanceToInteract = 2.5
 
