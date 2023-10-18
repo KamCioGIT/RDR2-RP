05881088fd77e5c94fe0a727bc453887595af2ec
levraimurphy <pellouinalexandre@gmail.com>
2023-10-18 21:33:25 +0200

    handsup

--- resources/dust_radialmenu/cfg.lua
+++ resources/dust_radialmenu/cfg.lua
@@ -4,3 +4,11 @@ Config.RagdollKey = 0x8CFFE0A1 -- w
 Config.HandsUpKey = 0x8CC9CD42 -- x 
 Config.MenuKey = 0x8AAA0AD4 -- b
 
+Config.HandsUpAnim = {
+[1] = 	{dict= "mech_loco_f@generic@reaction@handsup@unarmed@normal", anim = "loop"},
+[2] = 	{dict= "mech_loco_f@generic@reaction@handsup@unarmed@tough", anim = "loop"},
+[3] = 	{dict= "mech_loco_m@generic@reaction@handsup@unarmed@normal", anim = "loop"},
+[4] = 	{dict= "mech_loco_m@generic@reaction@handsup@unarmed@tough", anim = "loop"},
+[5] = 	{dict= "script_common@other@unapproved", anim = "guard_handsup_loop"},
+[6] = 	{dict= "script_proc@robberies@shop@strgen", anim ="handsup_register"},
+}
--- resources/dust_radialmenu/client.lua
+++ resources/dust_radialmenu/client.lua
@@ -8,12 +8,12 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
         local Player = PlayerPedId()
         
-        if not CanPedRagdoll(Player) or IsEntityDead(Player) or IsPedInAnyVehicle(Player, true) then
+        if not CanPedRagdoll(Player) or IsEntityDead(Player)then
             Ragdoll = false
         end
 
         if IsControlJustReleased(0, Config.RagdollKey) then
-            if Ragdoll or not CanPedRagdoll(Player) or IsEntityDead(Player) or IsPedInAnyVehicle(Player, false) then
+            if Ragdoll or not CanPedRagdoll(Player) or IsEntityDead(Player) then
                 Ragdoll = false
             else
                 Ragdoll = true
@@ -25,3 +25,31 @@ Citizen.CreateThread(function()
 		end
     end
 end)
+
+local HandsUp = false
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local Player = PlayerPedId()
+
+        if IsControlJustReleased(0, Config.HandsUpKey) then
+            if HandsUp then
+                ClearPedTasks(Player)
+                HandsUp = false
+            else
+                playAnimation()
+                HandsUp = true
+            end
+        end
+            
+    end
+end)
+
+function playAnimation()
+    key = math.random(1, #Config.HandsUpAnim)
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Citizen.Wait(0)
+    end
+    TaskPlayAnim(PlayerPedId(), Config.HandsUpAnim[key].dict, Config.HandsUpAnim[key].anim, 1.0, -1.0, -1, -1, 0, true, 0, false, 0, false)
+end
