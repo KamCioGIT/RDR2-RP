63869a5a9c56b8de425766b4db49ecc5ba0030b5
levraimurphy <pellouinalexandre@gmail.com>
2023-10-18 20:58:40 +0200

    Update client.lua

--- resources/dust_radialmenu/client.lua
+++ resources/dust_radialmenu/client.lua
@@ -2,6 +2,19 @@ RedEM = exports["redem_roleplay"]:RedEM()
 
 local Ragdoll = false
 
+Control = {
+    Toggle = 0x8CC9CD42, --X
+    Forward = 0x8FD015D8, -- W
+    Backward = 0xD27782E3, -- S
+    Left = 0x7065027D, -- A
+    Right = 0xB4E465B4, -- D
+    Up = 0xDE794E3E, -- Q
+    Down = 0x26E9DC00, -- Z
+    Increase = 0xE30CD707, -- R
+    Decrease = 0xB2F377E8, -- F
+    Mode = 0xCEFD9220, -- E
+}
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
