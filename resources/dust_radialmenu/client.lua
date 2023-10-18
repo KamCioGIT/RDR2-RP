5f122bd9165e7e8a3373e8f5ea073e21f31a5bd3
levraimurphy <pellouinalexandre@gmail.com>
2023-10-18 23:44:40 +0200

    Update client.lua

--- resources/dust_radialmenu/client.lua
+++ resources/dust_radialmenu/client.lua
@@ -72,37 +72,75 @@ end)
 
 
 ---- menu 
-exports('settingsRadialHandler', function(menu, item)
-    if menu == 'interactions' and item == 1 then
-        TriggerEvent("redemrp_inventory:SearchPlayer")
-    elseif menu == 'interactions' and item == 2 then
-        print 'give money'
-    elseif menu == 'interactions' and item == 3 then
-        print 'craft'
+exports('myMenuHandler', function(menu, item)
+    print(menu, item)
+ 
+    if menu == 'police_menu' and item == 1 then
+        print('Handcuffs')
     end
 end)
-
-
+ 
 lib.registerRadial({
-    id = 'interactions',
-    items = {
-        {
-            label = 'Fouiller',
-            icon = 'magnifying-glass',
-            onSelect = 'settingsRadialHandler'
-        },
-        {
-            label = "Donner de l'argent",
-            icon = 'money-bill-transfer',
-            onSelect = 'settingsRadialHandler'
-        },
-        {
-            label = 'Fabriquer',
-            icon = 'people-hammer',
-            onSelect = 'settingsRadialHandler'
-        },
+  id = 'police_menu',
+  items = {
+    {
+      label = 'Handcuff',
+      icon = 'handcuffs',
+      onSelect = 'myMenuHandler'
+    },
+    {
+      label = 'Frisk',
+      icon = 'hand'
+    },
+    {
+      label = 'Fingerprint',
+      icon = 'fingerprint'
+    },
+    {
+      label = 'Jail',
+      icon = 'bus'
+    },
+    {
+      label = 'Search',
+      icon = 'magnifying-glass',
+      onSelect = function()
+        print('Search')
+      end
     }
+  }
 })
-
-
-lib.disableRadial(false)
+ 
+lib.addRadialItem({
+  {
+    id = 'police',
+    label = 'Police',
+    icon = 'shield-halved',
+    menu = 'police_menu'
+  },
+  {
+    id = 'business_stuff',
+    label = 'Business',
+    icon = 'briefcase',
+    onSelect = function()
+      print("Business")
+    end
+  }
+})
+ 
+local coords = GetEntityCoords(cache.ped)
+local point = lib.points.new(coords, 5)
+ 
+function point:onEnter()
+  lib.addRadialItem({
+    id = 'garage_access',
+    icon = 'warehouse',
+    label = 'Garage',
+    onSelect = function()
+      print('Garage')
+    end
+  })
+end
+ 
+function point:onExit()
+  lib.removeRadialItem('garage_access')
+end
