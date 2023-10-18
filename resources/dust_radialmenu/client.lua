99d7a95d76fdc1295432356c6235eda4a54139d8
levraimurphy <pellouinalexandre@gmail.com>
2023-10-18 21:01:05 +0200

    Update client.lua

--- resources/dust_radialmenu/client.lua
+++ resources/dust_radialmenu/client.lua
@@ -24,7 +24,7 @@ Citizen.CreateThread(function()
             Ragdoll = false
         end
 
-        if IsControlJustReleased(0, 0xF3830D8E) then
+        if IsControlJustReleased(0, 0x8CC9CD42) then
             if Ragdoll or not CanPedRagdoll(Player) or IsEntityDead(Player) or IsPedInAnyVehicle(Player, false) then
                 Ragdoll = false
             else
