f3af6078e57c9d5590e449c6d80c7ded954d22cd
levraimurphy <pellouinalexandre@gmail.com>
2023-10-20 20:41:41 +0200

    Update cfg.lua

--- resources/[MyJobs]/dust_event/cfg.lua
+++ resources/[MyJobs]/dust_event/cfg.lua
@@ -14,7 +14,7 @@ Config.Tenue = {
 		[5] = {drawable = "hat_mr1_020_alt02a", albedo ="itejupia_0x02c30438", normal = "mlylimba_0x787407cb", material = "bflnapea_0x8066e340", palette = "dsvkmzra_0xf509c745", tint0 = 198, tint1 = 7, tint2 = 8}},
 		[2] = { [1] = {drawable = "accs_mr1_062_alt01", albedo ="accs_mr1_062_c3_001_ab", normal = "gdmpbeda_0x348b7ca3", material = "qkipjpdb_0x8e0b712a", palette = "zkbjoojb_0xd1476963", tint0 = 124, tint1 = 58, tint2 = 48},
 		[2] = {drawable = " belt_mr1_005_alt", albedo ="bwgcgthb_0x6e8ac7c8", normal = "ondauwja_0x7bb76c6b", material = "khtscjha_0xe915712f", palette = "dsvkmzra_0xf509c745", tint0 = 199, tint1 = 84, tint2 = 19},
-		[3] = {drawable = "comboshirt_mr1_002_alt01", albedo ="unusvkna_0xa1391163", normal = "zmhlacma_0x2e4b54a6", material = "xmnuqwra_0x79eaf0e4", palette = "ymwmqyaa_0xf93db0c8", tint0 = 26, tint1 = 21, tint2 = 16},
+		[3] = {drawable = "comboshirt_mr1_005", albedo ="klytqmaa_0x594d1d9e", normal = "wdufmata_0x3124ad67", material = "kvbxzofb_0x08fd2de4", palette = "sbzopvra_0x4101ed87", tint0 = 140, tint1 = 37, tint2 = 49},
 		[4] = {drawable = "hat_mr1_015", albedo ="fbjhbpea_0xf411153b", normal = "sykvvnda_0x062629b9", material = "rcqetoda_0x96f24d52", palette = "yadlkkza_0xfb71527b", tint0 = 12, tint1 = 21, tint2 = 33},
 		[5] = {drawable = "pantcombo_mr1_062", albedo ="esutnwea_0x7debb58e", normal = "ndulaeaa_0x8496814a", material = "hejfmcsa_0xbc54d6a9", palette = "dsvkmzra_0xf509c745", tint0 = 83, tint1 = 12, tint2 = 31}
 	},
@@ -28,7 +28,7 @@ Config.Tenue = {
 	
 		[4] = { [1] = {drawable = "accs_mr1_064_compressed", albedo ="dwtiodca_0x16979b1c", normal = "aezyzhha_0x75ff2206", material = "wssnvoba_0x10635f2c", palette = "alllhuqa_0xaa65d8a3", tint0 = 55, tint1 = 63, tint2 = 49},
 		[2] = {drawable = " belt_mr1_005_alt", albedo ="jlrgwqka_0xd44f5768", normal = "crtmwbpb_0x7f7a454b", material = "ttgpvnea_0xa94efba8", palette = "zkbjoojb_0xd1476963", tint0 = 97, tint1 = 50, tint2 = 7},
-		[3] = {drawable = "comboshirt_mr1_002_alt01", albedo ="bwgcgthb_0x6e8ac7c8", normal = "ondauwja_0x7bb76c6b", material = "khtscjha_0xe915712f", palette = "dsvkmzra_0xf509c745", tint0 = 199, tint1 = 84, tint2 = 19},
+		[3] = {drawable = "comboshirt_mr1_005", albedo ="klytqmaa_0x594d1d9e", normal = "wdufmata_0x3124ad67", material = "kvbxzofb_0x08fd2de4", palette = "sbzopvra_0x4101ed87", tint0 = 140, tint1 = 37, tint2 = 49},
 		[4] = {drawable = "hat_mr1_015", albedo ="fbjhbpea_0xf411153b", normal = "sykvvnda_0x062629b9", material = "rcqetoda_0x96f24d52", palette = "yadlkkza_0xfb71527b", tint0 = 35, tint1 = 42, tint2 = 19},
 		[5] = {drawable = "neckerchief_mr1_004_suvb_center", albedo ="fxzjvria_0x2d2b82fc", normal = "rcgbrrga_0x3a8dd7b0", material = "mqopuddb_0x8c5685e9", palette = "alllhuqa_0xaa65d8a3", tint0 = 46, tint1 = 46, tint2 = 46},
 		[6] = {drawable = "pantcombo_mr1_062", albedo ="yujknjba_0xe86eef3c", normal = "uzwczfbb_0x22fb75ad", material = "rjtdrica_0x8ef60744", palette = "dsvkmzra_0xf509c745", tint0 = 199, tint1 = 24, tint2 = 91}
