242c80a45a2bb6a1794991d1f59e4388869e176f
levraimurphy <pellouinalexandre@gmail.com>
2023-10-20 16:51:34 +0200

    Update client.lua

--- resources/[MyJobs]/dust_event/client.lua
+++ resources/[MyJobs]/dust_event/client.lua
@@ -10,9 +10,9 @@ Citizen.CreateThread(function()
         local playerPos = GetEntityCoords(PlayerPedId())
 
         if #(playerPos - Config.Vestiaire) < 10.0 then
-            Citizen.InvokeNative(0x2A32FAA57B937173,-1795314153, Config.Atelier, 0, 0, 0, 0, 0, 0, Config.DistanceToInteract, Config.DistanceToInteract, 0.1, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0) --DrawMarker
+            Citizen.InvokeNative(0x2A32FAA57B937173,-1795314153, Config.Vestiaire, 0, 0, 0, 0, 0, 0, Config.DistanceToInteract, Config.DistanceToInteract, 0.1, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0) --DrawMarker
         end
-        if #(playerPos - Config.Atelier) < Config.DistanceToInteract and not isInteracting then
+        if #(playerPos - Config.Vestiaire) < Config.DistanceToInteract and not isInteracting then
             tenueprompt:setActiveThisFrame(true)
             if IsControlJustPressed(2, 0x760A9C6F) and not isInteracting then 
                 TriggerServerEvent("redemrp_inventory:tenueevent")
