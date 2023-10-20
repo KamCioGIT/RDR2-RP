6dc8cc69e4baa618c5c4a75f0dc1ca878fadcc07
levraimurphy <pellouinalexandre@gmail.com>
2023-10-20 14:16:54 +0200

    Update client.lua

--- resources/dust_event/client.lua
+++ resources/dust_event/client.lua
@@ -4,9 +4,9 @@ RegisterCommand("testevent", function()
     a = 0
     while true do
         if a < 10 then
-            local ped = PlayerPedId()
             local coords = GetEntityCoords(ped)
             local modelhash = GetHashKey("G_M_Y_UniExConfeds_01")
+            LoadModel(modelhash)
             local xOffset = math.random(1, 15)
             local yOffset = math.random(1, 15)
             local zOffset = math.random(1, 15)
@@ -18,7 +18,19 @@ RegisterCommand("testevent", function()
 
             local enemy = Citizen.InvokeNative(0xD49F9B0955C367DE, modelhash, newccords, GetEntityHeading(ped), true, true)
             Citizen.InvokeNative(0x9F7794730795E019,enemy,116,true)
+            Citizen.InvokeNative(0x1794B4FCC84D812F, enemy, 1) -- SetEntityVisible
+            Citizen.InvokeNative(0x0DF7692B1D9E7BA7, enemy, 255, false) -- SetEntityAlpha
+            Citizen.InvokeNative(0x283978A15512B2FE, enemy, true) -- Invisible without
+            Citizen.InvokeNative(0x4AD96EF928BD4F9A, enemy) -- SetModelAsNoLongerNeeded
             a = a + 1
         else break end
     end
 end)
+
+LoadModel = function(model)
+    while not HasModelLoaded(model) do
+        RequestModel(model)
+        
+        Citizen.Wait(1)
+    end
+end
