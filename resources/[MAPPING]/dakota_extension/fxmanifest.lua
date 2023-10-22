85e480c2597ee4d33aaac9b7c8a843b5b18cf714
levraimurphy <pellouinalexandre@gmail.com>
2023-10-22 16:42:01 +0200

    ee

--- resources/[MAPPING]/dakota_extension/fxmanifest.lua
+++ resources/[MAPPING]/dakota_extension/fxmanifest.lua
@@ -1,7 +1,7 @@
-fx_version 'adamant'
-game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+fx_version "adamant"
+game "rdr3"
 
-this_is_a_map 'yes'
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+this_is_a_map "yes"
 
-file 'dakota_extension.ymap'
+lua54 "yes"
