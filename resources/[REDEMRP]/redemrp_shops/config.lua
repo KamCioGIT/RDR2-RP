eb89743488565a7b926688c46e3e1560e45ddb68
levraimurphy <pellouinalexandre@gmail.com>
2023-10-22 21:37:53 +0200

    Update config.lua

--- resources/[REDEMRP]/redemrp_shops/config.lua
+++ resources/[REDEMRP]/redemrp_shops/config.lua
@@ -5,15 +5,15 @@ Config.BlipScale = 0.2
 Config.EnableDistance = 2.0 
 Config.Zones = {
 
-	Basic = {
-		Items = {},
-		Legal = true,
-		Pos = {
-			{x = -324.28,   y = 801.51, z = 117.88},				
-		},
-		Spawn = {
-			{x = -323.18,   y = 802.81, z = 117.94},				
-		},
-	},
+	-- Basic = {
+	-- 	Items = {},
+	-- 	Legal = true,
+	-- 	Pos = {
+	-- 		{x = -324.28,   y = 801.51, z = 117.88},				
+	-- 	},
+	-- 	Spawn = {
+	-- 		{x = -323.18,   y = 802.81, z = 117.94},				
+	-- 	},
+	-- },
 
 }
