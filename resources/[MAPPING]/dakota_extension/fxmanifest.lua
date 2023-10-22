0af2dc39d41037f70f80eef3ee82093664e7119f
levraimurphy <pellouinalexandre@gmail.com>
2023-10-22 16:39:59 +0200

    ee

--- resources/[MAPPING]/dakota_extension/fxmanifest.lua
+++ resources/[MAPPING]/dakota_extension/fxmanifest.lua
@@ -2,8 +2,4 @@ fx_version 'adamant'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-dependency 'objectloader'
-
-file 'dakota_extension.xml'
-
-objectloader_map 'dakota_extension.xml'
+file 'dakota_extension.ymap'
