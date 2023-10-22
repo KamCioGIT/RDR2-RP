95100dbdab4016d5909e62726193cff2aad5c43f
levraimurphy <pellouinalexandre@gmail.com>
2023-10-22 13:08:34 +0200

    Update functions.lua

--- resources/[REDEMRP]/redemrp_clothes_store/utils/functions.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/utils/functions.lua
@@ -90,6 +90,25 @@ function CalculatePriceHat()
 		return price
 end
 
+function CalculatePriceMask()
+	local price = 0
+		if IsPedMale(PlayerPedId()) then
+				if OldHatCache["masks"].model ~= HatCache["masks"].model or OldHatCache["masks"].texture ~= HatCache["masks"].texture then
+					if HatCache["masks"].model > 0 then
+						price = price + Config.Price["masks"]
+					end
+				end
+		else
+				if OldHatCache["masks"].model ~= HatCache["masks"].model or OldHatCache["masks"].texture ~= HatCache["masks"].texture then
+					if HatCache["masks"].model > 0 then
+						price = price + Config.Price["masks"]
+					end
+				end
+		
+		end
+		return price
+end
+
 
 function NativeSetPedComponentEnabled(ped, componentHash, immediately, isMp)
     local categoryHash = NativeGetPedComponentCategory(not IsPedMale(ped), componentHash)
