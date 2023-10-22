1e205945c8b4f0a7c938025808e6343ed9f377b6
levraimurphy <pellouinalexandre@gmail.com>
2023-10-22 12:13:51 +0200

    add couleur

--- resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
@@ -1032,7 +1032,6 @@ function OpenHatMenu()
     }, function(data, menu)
 
     end, function(data, menu)
-        print (data.current.value)
         if data.current.value == "save" then
             print "ouais la zone"
             destory()
