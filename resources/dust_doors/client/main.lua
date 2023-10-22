9c4821b1e065c6bcecf01f1a266173d7056c1b09
levraimurphy <pellouinalexandre@gmail.com>
2023-10-22 19:15:39 +0200

    Update main.lua

--- resources/dust_doors/client/main.lua
+++ resources/dust_doors/client/main.lua
@@ -147,20 +147,53 @@ Citizen.CreateThread(function()
 	end
 end)
 
-Citizen.CreateThread(function()
+Citizen.CreateThread(function() 
 	while true do
-		Citizen.Wait(0)
-		local playerCoords = GetEntityCoords(PlayerPedId())
-		for k, v in ipairs(Config.Trapdoor) do
-
-			local object = GetClosestObjectOfType(playerCoords.x, playerCoords.y, playerCoords.z, 10.0, GetHashKey(v.doorID), false, false, false)
-			if object ~= 0 then
-				SetEntityRotation(object, 90.0, 0.0, 0.0, 2, true)
+		local player = PlayerPedId()
+		local pos = GetEntityCoords(player)
+		local bellrun = false
+		for component, data in pairs(Config.Trapdoor) do
+			if DoesObjectOfTypeExistAtCoords(pos, 40.0, GetHashKey(data.doorid)) then
+				if data.obj == nil then
+					data.obj = GetClosestObjectOfType(pos, 40.0, GetHashKey(data.doorid), false, false, false)
+					if IsControlJustReleased(0, 0x760A9C6F) then
+						if trapopen then
+							trapopen = true
+							print 'open'
+							SetEntityRotation(data.obj, -90.0, 0.0, 0.0, 0, true)
+						else
+							SetEntityRotation(data.obj, -90.0, 0.0, 0.0, 0, true)
+							trapopen = false
+						end
+					end
+				else
+					if data.bell then
+						local currentH = GetClockHours()
+						local currentM = GetClockMinutes()
+						if currentH == 12 and currentM >= 0 and currentM <=15 and bellrun == false then
+							bellrun = true
+							print("^4Start Animating Bell (no loop):^6 "..data.model.."|"..data.obj)
+							Citizen.InvokeNative(0xDC6D22FAB76D4874, data.obj, data.anim, data.dict, -1, true, true, false, 1.0, 0)
+							Citizen.Wait(15000)
+						else
+							Citizen.InvokeNative(0x786591D986DE9159, data.obj, data.anim, data.dict, 0)
+							bellrun = false
+						end
+					end
+				end
+			else
+				if data.obj ~= nil then
+					print("^4Stop Animating :^6 "..data.model.."|"..data.obj)
+					Citizen.InvokeNative(0x786591D986DE9159, data.obj, data.anim, data.dict, 0)
+					data.obj = nil
+					bellrun = false
+				end		
 			end
+
 		end
+		Citizen.Wait(2000)
 	end
 end)
-
 
 RegisterNetEvent('redemrp_doorlocks:changedoor')
 AddEventHandler('redemrp_doorlocks:changedoor', function(doorID, state)
