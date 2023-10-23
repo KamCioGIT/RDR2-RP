ddc5feb6c044c28675a81d9be06575cca9e01c9e
levraimurphy <pellouinalexandre@gmail.com>
2023-10-23 14:59:25 +0200

    Update client.lua

--- resources/[MyJobs]/dust_armurier/client.lua
+++ resources/[MyJobs]/dust_armurier/client.lua
@@ -54,6 +54,7 @@ Citizen.CreateThread(function()
                 end
             end
             for k, v in pairs(Config.CraftArme) do
+                print(v)
                 if #(playerPos - v) < 10.0 then
                     Citizen.InvokeNative(0x2A32FAA57B937173,-1795314153, v, 0, 0, 0, 0, 0, 0, Config.DistanceToInteract, Config.DistanceToInteract, 0.1, 128, 64, 0, 64, 0, 0, 2, 0, 0, 0, 0) --DrawMarker
                 end
