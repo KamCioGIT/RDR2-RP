b46f823b0ee120010fe990f8c15e142dc89b387d
levraimurphy <pellouinalexandre@gmail.com>
2023-10-23 20:16:27 +0200

    Update config.lua

--- resources/dust_telegram/config.lua
+++ resources/dust_telegram/config.lua
@@ -7,7 +7,7 @@ Config.postoffice = {
     { name = "Blackwater", blip = 1475382911, coords = vector3(-875.1723, -1328.7574, 43.9580), radius = 1.5 },
     { name = "Saint Denis", blip = 1475382911, coords = vector3(2747.2612, -1395.0283, 46.1831), radius = 1.5 },
     { name = "Rhodes", blip = 1475382911, coords = vector3(1225.51, -1293.999, 76.909), radius = 1.5 },
-
+    { name = "Van Horn", blip = 1475382911, coords = vector3(2986.006, 568.6345, 44.62764), radius = 1.5 },
 }
 
 
