b7fd59206a929cf251fcdd2d0095277692dfb417
levraimurphy <pellouinalexandre@gmail.com>
2023-10-24 17:28:52 +0200

    add port + apo

---
+++ resources/[MyJobs]/dust_apothicaire/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Atelier = {
+    vector3(2721.123, -1226.798, 50.36772), -- stdenis
+    vector3(2629.656, -1354.371, 49.04771), -- apo st denis
+    vector3(-782.6106, -1306.656, 43.79617) --- bla
+}
