927b3868b9be64272b0cb9ffc2322c346dbbd3ef
levraimurphy <pellouinalexandre@gmail.com>
2023-10-25 21:18:32 +0200

    Update config.lua

--- resources/[MyJobs]/dust_apothicaire/config.lua
+++ resources/[MyJobs]/dust_apothicaire/config.lua
@@ -14,7 +14,7 @@ Config.CraftAnim = {
     "base",
 }
 
-
+Config.WorkingTime = 5000
 Config.Atelier = {
     vector3(2721.123, -1226.798, 50.36772), -- stdenis
     vector3(2629.656, -1354.371, 49.04771), -- apo st denis
