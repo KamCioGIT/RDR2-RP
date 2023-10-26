ec41ffd96c32cdfad9463432e9869c9c27d1df83
levraimurphy <pellouinalexandre@gmail.com>
2023-10-26 18:22:25 +0200

    Update main.lua

--- resources/[REDEMRP]/redemrp_bossmenu/server/main.lua
+++ resources/[REDEMRP]/redemrp_bossmenu/server/main.lua
@@ -1,5 +1,11 @@
 RedEM = exports["redem_roleplay"]:RedEM()
 
+
+data = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    data = call
+end)
+
 SettingGrade = {}
 Duty = {}
 JobLedgers = {}
