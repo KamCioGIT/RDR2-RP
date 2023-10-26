88b32b4b987efe301817e030b83b1a5df4e41386
levraimurphy <pellouinalexandre@gmail.com>
2023-10-26 19:59:14 +0200

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -184,11 +184,11 @@ function OpenStable(menutype, stable)
             if _menutype == 'Ouvrir' then
                 if data.current.value then
                     TriggerServerEvent("dust_stable:server:askcomponents", data.current.value)
-                    Wait(500)
+                    Wait(10)
                     for k, v in pairs(horselist) do
-                        print(v.id)
                         if v.id == data.current.value then
                             print 'yes'
+                            print (v.type)
                             if v.type == "horse" then
                                 spawnhorse(v.race, v.name, v.id, v.stashid)
                             elseif v.type == "cart" then
