4340fa04a3e423954c409d6f4d4122d7d5347274
levraimurphy <pellouinalexandre@gmail.com>
2023-10-29 14:38:42 +0100

    Update config.lua

--- resources/[MyJobs]/dust_armurier/config.lua
+++ resources/[MyJobs]/dust_armurier/config.lua
@@ -493,5 +493,5 @@ Config.DamageModifier = {
 
     [-1569615261]                        = 1.5,          -- poings
     [-618550132]                        = 1.3,          -- knife
-    [710736342]                        = 0.9,          -- hunter hatchet
+    [710736342]                        = 0.7,          -- hunter hatchet
 }
