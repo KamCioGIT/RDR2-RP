b2a921e4feba597779865cafe4e718214b41c280
levraimurphy <pellouinalexandre@gmail.com>
2023-10-30 21:25:54 +0100

    ee

--- resources/[REDEMRP]/redemrp_inventory/server/main.lua
+++ resources/[REDEMRP]/redemrp_inventory/server/main.lua
@@ -2467,12 +2467,13 @@ end)
 
 
 RegisterServerEvent("redemrp_inventory:checkpoison")
-AddEventHandler("redemrp_inventory:checkpoison", function(source, name)
+AddEventHandler("redemrp_inventory:checkpoison", function(joueur, name)
     local _source = source
-    local Player = RedEM.GetPlayer(_source)
-    print (source, name)
+    local Player = joueur
+    print (Player)
     print "lancement"
     if Player then
+        print 'yyzavbz'
         local identifier = Player.GetIdentifier()
         local charid = Player.GetActiveCharacter()
         local player_inventory = Inventory[identifier .. "_" .. charid]
