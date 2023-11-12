11c499383d2d4b234946890789b55e287b98aed4
levraimurphy <pellouinalexandre@gmail.com>
2023-11-12 16:10:42 +0100

    add dsitillerie

---
+++ resources/[MyJobs]/dust_distillerie/server.lua
@@ -0,0 +1,159 @@
+RedEM = exports["redem_roleplay"]:RedEM()
+
+data = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    data = call
+end)
+
+RegisterServerEvent('distillerie:CraftItem')
+AddEventHandler('distillerie:CraftItem', function(itemNameStr, menu, amount)
+	local _source = tonumber(source)
+	
+	for i = 1, amount, 1 do
+		TriggerEvent("redemrp_inventory:getData", function(Inventory)
+			if Config.CraftingsReceipe[itemNameStr].Itemtocraft == 1 then
+				local ItemData = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe1Name)
+				if ItemData.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe1Amount) then
+					local ItemDatagive = data.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemToGive)
+					ItemDatagive.AddItem(Config.CraftingsReceipe[itemNameStr].Amount)
+				end
+			end
+			if Config.CraftingsReceipe[itemNameStr].Itemtocraft == 2 then
+				local ItemData = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe1Name)
+				local ItemData2 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe2Name)
+				if ItemData.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe1Amount) and ItemData2.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe2Amount) then
+					local ItemDatagive = data.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemToGive)
+					ItemDatagive.AddItem(Config.CraftingsReceipe[itemNameStr].Amount)
+				end
+			end
+			if Config.CraftingsReceipe[itemNameStr].Itemtocraft == 3 then
+				local ItemData = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe1Name)
+				local ItemData2 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe2Name)
+				local ItemData3 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe3Name)
+				if ItemData.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe1Amount) and ItemData2.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe2Amount) and ItemData3.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe3Amount) then
+					local ItemDatagive = data.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemToGive)
+					ItemDatagive.AddItem(Config.CraftingsReceipe[itemNameStr].Amount)
+				end
+			end
+			if Config.CraftingsReceipe[itemNameStr].Itemtocraft == 4 then
+				local ItemData = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe1Name)
+				local ItemData2 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe2Name)
+				local ItemData3 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe3Name)
+				local ItemData4 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe4Name)
+				if ItemData.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe1Amount) and ItemData2.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe2Amount) and ItemData3.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe3Amount) and ItemData4.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe4Amount) then
+					local ItemDatagive = data.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemToGive)
+					ItemDatagive.AddItem(Config.CraftingsReceipe[itemNameStr].Amount)
+				end
+			end
+			if Config.CraftingsReceipe[itemNameStr].Itemtocraft == 5 then
+				local ItemData = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe1Name)
+				local ItemData2 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe2Name)
+				local ItemData3 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe3Name)
+				local ItemData4 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe4Name)
+				local ItemData5 = Inventory.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemReceipe5Name)
+				if ItemData.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe1Amount) and ItemData2.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe2Amount) and ItemData3.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe3Amount) and ItemData4.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe4Amount) and ItemData5.RemoveItem(Config.CraftingsReceipe[itemNameStr].ItemReceipe5Amount) then
+					local ItemDatagive = data.getItem(_source, Config.CraftingsReceipe[itemNameStr].ItemToGive)
+					ItemDatagive.AddItem(Config.CraftingsReceipe[itemNameStr].Amount)
+				end
+			end
+		end)
+				TriggerClientEvent("distillerie:CraftingAction", _source)
+
+		Citizen.Wait(Config.WorkingTime)
+	end
+end)
+
+RegisterServerEvent('distillerie:AddItem')
+AddEventHandler('distillerie:AddItem', function(item, amount)
+	print (item, amount)
+	local _source = tonumber(source)
+	local ItemData = data.getItem(_source, item)
+	ItemData.AddItem(amount)
+
+end)
+
+
+RegisterServerEvent("distillerie:MaxRessourcesAmount", function(dataType)
+    local _source = tonumber(source)
+    
+	if Config.CraftingsReceipe[dataType].Itemtocraft == 1 then
+		local ItemData = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe1Name)
+		
+		local rItem1Amount = tonumber(ItemData.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe1Amount)
+		
+		if rItem1Amount >= 1 then
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, math.floor(rItem1Amount))
+		else 
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, 0)
+		end
+	elseif Config.CraftingsReceipe[dataType].Itemtocraft == 2 then
+		local ItemData = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe1Name)
+		local ItemData2 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe2Name)
+		
+		local rItem1Amount = tonumber(ItemData.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe1Amount)
+		local rItem2Amount = tonumber(ItemData2.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe2Amount)
+		
+		if rItem1Amount >= rItem2Amount then
+			hAmount = rItem1Amount
+			lAmount = rItem2Amount
+		else
+			lAmount = rItem1Amount
+			hAmount = rItem2Amount
+		end
+	
+		if hAmount >= 1 and lAmount >= 1 then
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, math.floor(lAmount))
+		else 
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, 0)
+		end
+	elseif Config.CraftingsReceipe[dataType].Itemtocraft == 3 then
+		local ItemData = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe1Name)
+		local ItemData2 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe2Name)
+		local ItemData3 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe3Name)
+		
+		local Item1Amount = tonumber(ItemData.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe1Amount)
+		local Item2Amount = tonumber(ItemData2.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe2Amount)
+		local Item3Amount = tonumber(ItemData3.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe3Amount)
+
+		local lAmount = Item1Amount
+		if Item2Amount < lAmount then
+			lAmount = Item2Amount
+		end
+		if Item3Amount < lAmount then
+			lAmount = Item3Amount
+		end
+		if Item1Amount >= 1 and Item2Amount >= 1 and Item3Amount >= 1 then
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, math.floor(lAmount))
+		else
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, 0)
+		end
+	elseif Config.CraftingsReceipe[dataType].Itemtocraft == 4 then
+		local ItemData = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe1Name)
+		local ItemData2 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe2Name)
+		local ItemData3 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe3Name)
+		local ItemData4 = data.getItem(_source, Config.CraftingsReceipe[dataType].ItemReceipe4Name)
+		
+		local Item1Amount = tonumber(ItemData.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe1Amount)
+		local Item2Amount = tonumber(ItemData2.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe2Amount)
+		local Item3Amount = tonumber(ItemData3.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe3Amount)
+		local Item4Amount = tonumber(ItemData4.ItemAmount / Config.CraftingsReceipe[dataType].ItemReceipe4Amount)
+
+		local lAmount = Item1Amount
+		if Item2Amount < lAmount then
+			lAmount = Item2Amount
+		end
+		if Item3Amount < lAmount then
+			lAmount = Item3Amount
+		end
+		if Item4Amount < lAmount then
+			lAmount = Item4Amount
+		end
+		if Item1Amount >= 1 and Item2Amount >= 1 and Item3Amount >= 1 and Item4Amount >= 1 then
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, math.floor(lAmount))
+		else
+			TriggerClientEvent("distillerie:client:SetMaxAmount", _source, 0)
+		end
+	end
+
+end)
+
