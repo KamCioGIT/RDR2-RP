78fe4d9bcfc8cfea491bf8e4a7af504dfdbe5dac
levraimurphy <pellouinalexandre@gmail.com>
2023-11-13 11:36:23 +0100

    add mapping

---
+++ resources/[MAPPING]/distil_int/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+dependency 'objectloader'
+
+file 'distil_int.xml'
+
+objectloader_map 'distil_int.xml'
