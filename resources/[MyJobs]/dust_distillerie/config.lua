e9ada36e16567e8fb6325bcb1ffc72a1790ad8de
levraimurphy <pellouinalexandre@gmail.com>
2023-11-14 19:36:10 +0100

    Update config.lua

--- resources/[MyJobs]/dust_distillerie/config.lua
+++ resources/[MyJobs]/dust_distillerie/config.lua
@@ -8,8 +8,8 @@ Config.WorkingTime = 5000
 Config.DistanceToInteract = 2.0
 
 Config.Atelier = vector3(1461.654, -1591.384, 70.90051) -- Creer toutes les balles
-Config.Dep = vector3(1471.082, -1582.883, 71.18633)
-Config.Ret = vector3(1466.216, -1581.007, 70.91117)
+Config.Dep = {vector3(1471.082, -1582.883, 71.18633)}
+Config.Ret = {vector3(1466.216, -1581.007, 70.91117)}
 Config.AnimDict = "amb_work@world_human_hammer@table@male_a@trans"
 Config.CraftAnim = {
     "base_trans_base",
