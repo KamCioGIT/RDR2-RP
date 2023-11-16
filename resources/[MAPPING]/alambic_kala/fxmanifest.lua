013d5a55fc3fa6f576d28af5c64ca499a62f08a4
levraiwade <jean-genicot@hotmail.fr>
2023-11-16 19:19:34 +0100

    a

---
+++ resources/[MAPPING]/alambic_kala/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+dependency 'objectloader'
+
+file 'alambic_kala.xml'
+
+objectloader_map 'alambic_kala.xml'
