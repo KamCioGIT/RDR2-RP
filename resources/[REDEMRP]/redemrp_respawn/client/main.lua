412224db9c824daccdf103948251ebdd46504f50
levraimurphy <pellouinalexandre@gmail.com>
2023-11-18 16:55:11 +0100

    Update main.lua

--- resources/[REDEMRP]/redemrp_respawn/client/main.lua
+++ resources/[REDEMRP]/redemrp_respawn/client/main.lua
@@ -93,33 +93,23 @@ RegisterNetEvent("redemrp_respawn:client:Revived", function(c)
     --Citizen.InvokeNative(0xFDB74C9CC54C3F37, 0.0)
     DestroyAllCams(true)
     Wait(1000)
-    UpdateHealthRevive()
     TriggerEvent("redemrp_respawn:respawnCoords", GetEntityCoords(PlayerPedId()))
     TriggerServerEvent("RedEM:server:LoadSkin")
+    Wait(300)
+    UpdateHealthRevive()
 end)
 
 function UpdateHealthRevive()
-        local buf1 = DataView.ArrayBuffer(8 * 12)
-        local buf2 = DataView.ArrayBuffer(8 * 12)
-        for j=1, 12-1 do
-            buf1:SetInt32(j * 8, 0)
-            buf2:SetInt32(j * 8, 0)
-        end
-    
-    
         local ped = PlayerPedId()
         local pid = PlayerId()
-    
-        Citizen.InvokeNative(0xCB5D11F9508A928D, 1, buf1:Buffer(), buf2:Buffer(), `UPGRADE_HEALTH_TANK_1`, 0x1084182731, 1, 0x752097756)
-        Citizen.InvokeNative(0xCB5D11F9508A928D, 1, buf1:Buffer(), buf2:Buffer(), `UPGRADE_STAMINA_TANK_1`, 0x1084182731, 1, 0x752097756)
-    
+
         Citizen.InvokeNative(0xC6258F41D86676E0, ped, 0, 10) -- _SET_ATTRIBUTE_CORE_VALUE
         Citizen.InvokeNative(0xC6258F41D86676E0, ped, 1, 10) -- _SET_ATTRIBUTE_CORE_VALUE
     
     
         SetEntityHealth(ped, 10)
-        Citizen.InvokeNative(0xA9EC16C7, pid, Citizen.InvokeNative(0xD014AB79, pid)) -- SetPlayerStamina & GetPlayerMaxStamina
-        Citizen.InvokeNative(0x675680D089BFA21F, PlayerPedId(), 100.0)
+        Citizen.InvokeNative(0xA9EC16C7, pid, 10) -- SetPlayerStamina & GetPlayerMaxStamina
+        Citizen.InvokeNative(0x675680D089BFA21F, PlayerPedId(), 10.0)
 end
 
 RegisterCommand("revive", function(source, args, rawCommand)
