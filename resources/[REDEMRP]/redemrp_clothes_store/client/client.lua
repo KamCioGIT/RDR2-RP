2a2397fbece3ee851f769c5595b1745472ae3236
levraimurphy <pellouinalexandre@gmail.com>
2023-11-20 17:27:38 +0100

    Update client.lua

--- resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
@@ -2472,8 +2472,7 @@ function ChangeDBagues(id, change_type, target)
                 --     UpdateCustomClothes(target, drawable, albedo, normal, material, palette, tint0, tint1, tint2)
                 -- else
                 print"getchange"
-                NativeSetPedComponentEnabled(target,
-                3881958970, false, true, true)
+                NativeSetPedComponentEnabled(target, 3881958970, true, true, true)
                 -- end
             end
         end
