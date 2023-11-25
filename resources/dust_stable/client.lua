0193607f243afd3d9b1c354bddc7e3d6be0cf1ac
levraimurphy <pellouinalexandre@gmail.com>
2023-11-25 15:23:03 +0100

    Update client.lua

--- resources/dust_stable/client.lua
+++ resources/dust_stable/client.lua
@@ -913,7 +913,6 @@ Citizen.CreateThread(function()
         local cart = lib.getClosestVehicle(coords, 3.0, false)
         if cart then
             if Entity(cart).state.stashid then
-                print'yes'
                 if GetEntityModel(cart) == -1698498246 then
                     local holding = Citizen.InvokeNative(0xD806CD2A4F2C2996, PlayerPedId())
                     local hold = GetPedType(holding)
