6eb8716bca74179cfb2aed322ec2b068e381eb7d
levraiwade <jean-genicot@hotmail.fr>
2023-11-25 18:10:06 +0100

    Update test.lua

--- resources/[REDEMRP]/redemrp_clothes_store/utils/test.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/utils/test.lua
@@ -78711,78 +78711,6 @@ clothes_list = {
 					['tint2'] = 255
 				}
 			},
-			[53] = {
-                [1] = {
-                    ['category_hash'] = 2727505819,
-                    ['category_hash_dec_signed'] = -1567461477,
-                    ['category_hashname'] = 'coats',
-                    ['hashname'] = '',
-                    ['is_multiplayer'] = false,
-                    ['ped_type'] = 'male',
-                    ['drawable'] = coat_mr1_130,
-                    ['albedo'] = `kkwecyia_0xb2e66aba`,
-                    ['normal'] = `iyphvbwa_0x2089387a`,
-                    ['material'] = `cjdntsua_0x5462f8e3`,
-                    ['palette'] = `alllhuqa_0xaa65d8a3`,
-                    ['tint0'] = 159,
-                    ['tint1'] = 170,
-                    ['tint2'] = 150
-                }
-            },
-			[54] = {
-                [1] = {
-                    ['category_hash'] = 2727505819,
-                    ['category_hash_dec_signed'] = -1567461477,
-                    ['category_hashname'] = 'coats',
-                    ['hashname'] = '',
-                    ['is_multiplayer'] = false,
-                    ['ped_type'] = 'male',
-                    ['drawable'] = `cs_henrilemiux_ms1_coat_000`,
-                    ['albedo'] = `cailcwpa_0xfc71f201`,
-                    ['normal'] = `xdvbncsa_0x657df503`,
-                    ['material'] = `nixnahda_0xee170645`,
-                    ['palette'] = ``,
-                    ['tint0'] = 0,
-                    ['tint1'] = 0,
-                    ['tint2'] = 0
-                }
-            },
-			[55] = {
-                [1] = {
-                    ['category_hash'] = 2727505819,
-                    ['category_hash_dec_signed'] = -1567461477,
-                    ['category_hashname'] = 'coats',
-                    ['hashname'] = '',
-                    ['is_multiplayer'] = false,
-                    ['ped_type'] = 'male',
-                    ['drawable'] = `cs_archerfordham_ms1_coat_000`,
-                    ['albedo'] = `fpfplwja_0xa2a659b1`,
-                    ['normal'] = `spteixha_0xaac9d974`,
-                    ['material'] = `jrwxwlxa_0x67a19c7d`,
-                    ['palette'] = ``,
-                    ['tint0'] = 0,
-                    ['tint1'] = 0,
-                    ['tint2'] = 0
-                }
-            },
-			[56] = {
-                [1] = {
-                    ['category_hash'] = 2727505819,
-                    ['category_hash_dec_signed'] = -1567461477,
-                    ['category_hashname'] = 'coats',
-                    ['hashname'] = '',
-                    ['is_multiplayer'] = false,
-                    ['ped_type'] = 'male',
-                    ['drawable'] = `cs_hamish_ms1_coat_000`,
-                    ['albedo'] = `xeykzbab_0x7017878e`,
-                    ['normal'] = `ndartzva_0xb9ae78d3`,
-                    ['material'] = `sznekqoa_0x052a7639`,
-                    ['palette'] = ``,
-                    ['tint0'] = 0,
-                    ['tint1'] = 0,
-                    ['tint2'] = 0
-                }
-            },
 		},
 		['coats_closed'] = {
 			[1] = {
@@ -81052,25 +80980,7 @@ clothes_list = {
 					['is_multiplayer'] = true,
 					['ped_type'] = 'male'
 				}
-			},
-			[20] = {
-				[1] = {
-					['category_hash'] = 2727505819,
-					['category_hash_dec_signed'] = -1567461477,
-					['category_hashname'] = 'coats_closed',
-					['hashname'] = '',
-					['is_multiplayer'] = false,
-					['ped_type'] = 'male',
-					['drawable'] = `coat_mr1_104`,
-					['albedo'] = `iphpgvxa_0x656df394`,
-					['normal'] = `oxeruosa_0x32876b90`,
-					['material'] = `ecxeckea_0x221d1386`,
-					['palette'] = `ymwmqyaa_0xf93db0c8`,
-					['tint0'] = 139,
-					['tint1'] = 200,
-					['tint2'] = 190
-				}
-			},
+			}
 		},
 		['dresses'] = {
 			[1] = {
