d5667129ab2a01ef0a16bf605f007cf6dcee428c
levraiwade <jean-genicot@hotmail.fr>
2023-11-25 14:55:23 +0100

    Update test.lua

--- resources/[REDEMRP]/redemrp_clothes_store/utils/test.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/utils/test.lua
@@ -114705,24 +114705,6 @@ clothes_list = {
 					['tint2'] = 0
 				}
 			},
-			[39] = {
-				[1] = {
-					['category_hash'] = 539411565,
-					['category_hash_dec_signed'] = 539411565,
-					['category_hashname'] = 'shirts_full',
-					['hashname'] = '',
-					['is_multiplayer'] = false,
-					['ped_type'] = 'male',
-					['drawable'] = `coat_mr1_104`,
-					['albedo'] = `iphpgvxa_0x656df394`,
-					['normal'] = `oxeruosa_0x32876b90`,
-					['material'] = `ecxeckea_0x221d1386`,
-					['palette'] = `ymwmqyaa_0xf93db0c8`,
-					['tint0'] = 139,
-					['tint1'] = 200,
-					['tint2'] = 190
-				}
-			},
 		},
 		['spats'] = {
 			[1] = {
@@ -119848,7 +119830,25 @@ clothes_list = {
 					['is_multiplayer'] = true,
 					['ped_type'] = 'male'
 				}
-			}
+			},
+			[29] = {
+				[1] = {
+					['category_hash'] = 1214179380,
+					['category_hash_dec_signed'] = 1214179380,
+					['category_hashname'] = 'vests',
+					['hashname'] = '',
+					['is_multiplayer'] = false,
+					['ped_type'] = 'male',
+					['drawable'] = `coat_mr1_104`,
+					['albedo'] = `iphpgvxa_0x656df394`,
+					['normal'] = `oxeruosa_0x32876b90`,
+					['material'] = `ecxeckea_0x221d1386`,
+					['palette'] = `ymwmqyaa_0xf93db0c8`,
+					['tint0'] = 139,
+					['tint1'] = 200,
+					['tint2'] = 190
+				}
+			},
 		}
 	}
 }
