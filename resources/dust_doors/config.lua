55e95f60b98dafef439e872f63ec38710b10a7ea
levraimurphy <pellouinalexandre@gmail.com>
2023-11-28 17:16:14 +0100

    Update config.lua

--- resources/dust_doors/config.lua
+++ resources/dust_doors/config.lua
@@ -1766,7 +1766,7 @@ Config.Doors = {
 
     {
         door = 2279482544,
-        CloseRotation = 90,
+        CloseRotation = 85,
         OpenCoord  = vector3(-1439.96, -2275.91, 42.5014458),
         door2 = 0,
         CloseRotation2 = 0,
