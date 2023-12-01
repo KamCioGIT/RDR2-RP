a8b549f5153e81e86e49710ab054a80edf447e19
levraiwade <jean-genicot@hotmail.fr>
2023-12-01 20:27:12 +0100

    a

--- resources/dust_rumors/cfg.lua
+++ resources/dust_rumors/cfg.lua
@@ -7,6 +7,9 @@ Config.ShareRumorSpot = {
     vector3(2753.191, -1384.629, 47.23839), -- SAINT DENIS
     vector3(2931.716, 1302.213, 46.10941), -- ANNESBOURG
     vector3(2992.194, 569.4253, 44.47285), -- van horn
+    vector3(-1094.919, -569.6492, 81.40599), -- riggs
+    vector3(-1295.615, 389.4035, 94.24384), -- wallace
+    vector3(1523.401, 451.8167, 89.3623), -- emerald
 }
 
 Config.RumorPrice = 0.1
--- resources/dust_telegram/config.lua
+++ resources/dust_telegram/config.lua
@@ -10,7 +10,9 @@ Config.postoffice = {
     { name = "Van Horn", blip = 1475382911, coords = vector3(2986.006, 568.6345, 44.62764), radius = 1.5 },
     { name = "Strawberry", blip = 1475382911, coords =  vector3(-1767.434, -381.4054, 157.7319), radius = 1.5 },
     { name = "Annesburg", blip = 1475382911, coords = vector3(2939.433, 1288.489, 44.65283), radius = 1.5 },
-
+    { name = "Riggs", blip = 1475382911, coords = vector3(-1095.529, -576.5807, 81.40778), radius = 1.5 },
+    { name = "Wallace", blip = 1475382911, coords = vector3(-1301.196, 398.6781, 94.42928), radius = 1.5 },
+    { name = "Emerald", blip = 1475382911, coords = vector3(1521.912, 439.5645, 89.68072), radius = 1.5 },
 }
 
 
