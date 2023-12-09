9ae6789237db38322bb1f35783cbe756a7f596d4
levraiwade <jean-genicot@hotmail.fr>
2023-12-09 20:13:07 +0100

    a

--- resources/[MAPPING]/grotte_illumine/fxmanifest.lua
+++ resources/[MAPPING]/grotte_illumine/fxmanifest.lua
@@ -1,9 +1,7 @@
-fx_version 'adamant'
-game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+fx_version "adamant"
+game "rdr3"
 
-dependency 'objectloader'
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+this_is_a_map "yes"
 
-file 'grotte_illumine.xml'
-
-objectloader_map 'grotte_illumine.xml'
+lua54 "yes"
