f3c3e447afbc701332a08a59eca9804fbfe440f7
levraimurphy <pellouinalexandre@gmail.com>
2023-12-11 20:26:53 +0100

    e

---
+++ resources/[MAPPING]/tabac/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+dependency 'objectloader'
+
+file {
+    'tabac1.xml',
+    'tabac2.xml'
+
+}
+
+objectloader_map {
+    'tabac1.xml',
+    'tabac2.xml',
+}
