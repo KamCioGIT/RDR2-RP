0a5633a12fd380e523ac81cfc3542592f8695f79
levraimurphy <pellouinalexandre@gmail.com>
2024-01-20 14:37:38 +0100

    e

--- resources/redem_roleplay/client/cl_entities.lua
+++ resources/redem_roleplay/client/cl_entities.lua
@@ -43,3 +43,35 @@ RegisterNetEvent("redemrp:manual_entity_update", function()
     local p_coords = GetEntityCoords(PlayerPedId())
     TriggerServerEvent("redemrp:request_entities", p_coords.x, p_coords.y)    
 end)
+
+Citizen.CreateThread(function()
+    local vehiclePool = {}
+    local wagon = 0
+    local driver = 0
+    local horse = 0
+    while true do
+        vehiclePool = GetGamePool('CVehicle') -- Get the list of vehicles (entities) from the pool
+        for i = 1, #vehiclePool do -- loop through each vehicle (entity)
+            wagon = vehiclePool[i]
+            -- Is wagon stopped
+            if IsEntityAVehicle(wagon) and IsVehicleStopped(wagon) then
+                -- Get the horse
+                horse = Citizen.InvokeNative(0xA8BA0BAE0173457B,wagon,0)
+                -- If vehicle stopped but the horse walks = buggy wagon
+                if IsPedWalking(horse) then
+                    if not IsEntityAMissionEntity(wagon) then --if the wagon is not a mission entity. Peds spawned naturally by game return false, peds created by script return true by default preventing this from deleting entities intentionally spawned by scripts
+                        -- Delete driver & wagon
+                        driver = Citizen.InvokeNative(0x2963B5C1637E8A27,wagon)
+                        if driver ~= PlayerPedId() then -- Ensure the driver is not a player
+                            if driver then
+                                DeleteEntity(driver) -- Delete driver from wagon if there is one
+                            end
+                            DeleteEntity(wagon) -- Delete buggy wagon
+                        end
+                    end
+                end
+            end
+        end
+        Citizen.Wait(1000)
+    end
+end)
