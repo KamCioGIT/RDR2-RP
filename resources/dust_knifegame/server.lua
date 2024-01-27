f081240e4243cdfd18df29d195e5f26eedcf9134
levraimurphy <pellouinalexandre@gmail.com>
2024-01-28 00:05:02 +0100

    Update server.lua

--- resources/dust_knifegame/server.lua
+++ resources/dust_knifegame/server.lua
@@ -1,6 +1,7 @@
 
-RegisterCommand(Config.Knife_Play, function(source, args)
-	TriggerClientEvent('nicoo_play',source,1)
+RegisterCommand(Config.Knife_Play, function(args)
+	local _source = source
+	TriggerClientEvent('dust_play',_source,1)
 end)
 
 
