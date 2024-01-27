4801b322b745e46122f609663bd5f14b9930ae95
levraimurphy <pellouinalexandre@gmail.com>
2024-01-27 22:01:06 +0100

    Update client.lua

--- resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
+++ resources/[REDEMRP]/redemrp_clothes_store/client/client.lua
@@ -2304,7 +2304,6 @@ end)
 local bandana = false
 RegisterNetEvent('dust:bandana', function(source, args, rawCommand)
     cache_comps = json.decode(comps)
-    print(ClothesCache["NeckWear"].model)
     if not bandana then 
         Citizen.InvokeNative(0xAE72E7DF013AAA61, PlayerPedId(), clothes_list["female"]["neckties"][ClothesCache["neckties"].model][ClothesCache["neckties"].texture].hash, GetHashKey("BANDANA_ON_RIGHT_HAND"), 1, 0, -1082130432)
         Wait(700)
