0fd99d117701fee863b8dc8547fbb52f1ceb9819
levraimurphy <pellouinalexandre@gmail.com>
2024-01-28 11:04:06 +0100

    e

--- resources/gum_notepad/client.lua
+++ resources/gum_notepad/client.lua
@@ -1,14 +1,14 @@
 local mymessage = {}
 
-RegisterNetEvent("gum_notepad:get_notepad")
-AddEventHandler("gum_notepad:get_notepad", function(tbl)
+RegisterNetEvent("dust_notepad:get_notepad")
+AddEventHandler("dust_notepad:get_notepad", function(tbl)
 	mymessage = tbl
 end)
 
-RegisterNetEvent("gum:SelectedCharacter")
-AddEventHandler("gum:SelectedCharacter", function(charid)
+RegisterNetEvent("dust:SelectedCharacter")
+AddEventHandler("dust:SelectedCharacter", function(charid)
 	Citizen.CreateThread(function()
-		TriggerServerEvent("gum_notepad:get_notepad")
+		TriggerServerEvent("dust_notepad:get_notepad")
 	end)
 end)
 
@@ -21,7 +21,7 @@ RegisterNUICallback('exit', function(data, cb)
 end)
 
 RegisterCommand("notepad", function()
-	TriggerServerEvent("gum_notepad:get_notepad")
+	TriggerServerEvent("dust_notepad:get_notepad")
 	Citizen.Wait(1000)
 	SendNUIMessage({
         type = "notepad",
@@ -34,28 +34,28 @@ end)
 RegisterNUICallback('save', function(data, cb)
 	SetNuiFocus(false, false)
 	SendNUIMessage({type = "notepad",status = false,})
-	TriggerServerEvent("gum_notepad:save", data.name, data.value)
+	TriggerServerEvent("dust_notepad:save", data.name, data.value)
 end)
 
 RegisterNUICallback('save_new', function(data, cb)
 	SetNuiFocus(false, false)
 	SendNUIMessage({type = "notepad",status = false,})
-	TriggerServerEvent("gum_notepad:save_new", data.value)
+	TriggerServerEvent("dust_notepad:save_new", data.value)
 end)
 
 RegisterNUICallback('delete', function(data, cb)
 	SetNuiFocus(false, false)
 	SendNUIMessage({type = "notepad",status = false,})
-	TriggerServerEvent("gum_notepad:delete", data.name)
+	TriggerServerEvent("dust_notepad:delete", data.name)
 end)
 
 RegisterNUICallback('send', function(data, cb)
     SetNuiFocus(false, false)
 	SendNUIMessage({type = "notepad",status = false,})
-	TriggerEvent("guminputs:getInput", "Potvrdit", "Vlož ID hráče kterému to chceš poslat", function(cb)
+	TriggerEvent("dustinputs:getInput", "Potvrdit", "Vlož ID hráče kterému to chceš poslat", function(cb)
 		local id_player = tonumber(cb)
 		if id_player ~= "close" and id_player ~= nil then
-			TriggerServerEvent("gum_notepad:send", id_player, data.value)
+			TriggerServerEvent("dust_notepad:send", id_player, data.value)
 		end
 	end)
 end)
